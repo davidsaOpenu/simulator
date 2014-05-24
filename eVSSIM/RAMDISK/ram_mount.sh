c07e767a81e8b37b58447dc9d0ee15e01bcc8667
Boaz Adato <boazadato@gmail.com>
2014-05-24 19:23:49 +0300

    new run script for qemu removed data/*dat files from repo added QEMU/x86_64-softmmu/os_img_path.conf for run script configuraion for os file

--- eVSSIM/RAMDISK/ram_mount.sh
+++ eVSSIM/RAMDISK/ram_mount.sh
@@ -4,14 +4,17 @@ path=`readlink -e $path`
 if [ 0 -eq `mount | grep "$path/rd" -c` ]; then 
 	echo "mounting $path"
 	sudo mount -t tmpfs -o size=16g tmpfs ./rd
-else
-	echo "$path already mounted"
+#else
+	#echo "$path already mounted"
 fi
-sudo chmod 0777 rd 
 
-ram_image='rd/ssd.img'
+rd_path="$path/rd"
+
+sudo chmod 0777 $rd_path
+
+ram_image=$rd_path'/ssd.img'
 if [ -f $ram_image ]; then
-	echo "ram image $ram_image already exist"
+	#echo "ram image $ram_image already exist"
 else
 	echo "creating ram image $ram_image"
 	../QEMU/qemu-img create -f qcow $ram_image 16G
