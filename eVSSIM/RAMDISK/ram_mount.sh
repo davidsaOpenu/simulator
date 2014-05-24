001bf8043d6ab39c810c99611bf8774732c8264d
Boaz Adato <boazadato@gmail.com>
2014-05-24 18:47:07 +0300

    ram_mount:  now also creates the ssd image now is idempotent

--- eVSSIM/RAMDISK/ram_mount.sh
+++ eVSSIM/RAMDISK/ram_mount.sh
@@ -1,3 +1,19 @@
-mkdir rd
-chmod 0755 rd
-sudo mount -t tmpfs -o size=16g tmpfs ./rd
+sudo mkdir rd -p
+path=`dirname $0`
+path=`readlink -e $path`
+if [ 0 -eq `mount | grep "$path/rd" -c` ]; then 
+	echo "mounting $path"
+	sudo mount -t tmpfs -o size=16g tmpfs ./rd
+else
+	echo "$path already mounted"
+fi
+sudo chmod 0777 rd 
+
+ram_image='rd/ssd.img'
+if [ -f $ram_image ]; then
+	echo "ram image $ram_image already exist"
+else
+	echo "creating ram image $ram_image"
+	../QEMU/qemu-img create -f qcow $ram_image 16G
+	echo "DONE"
+fi
