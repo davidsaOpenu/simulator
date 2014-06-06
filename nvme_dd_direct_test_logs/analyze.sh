a7988475651038e2e2b2e37b33ec9a8d829462b6
Boaz Adato <boazadato@gmail.com>
2014-06-06 21:39:13 +0300

    analyze results (format for markdown)

--- nvme_dd_direct_test_logs/analyze.sh
+++ nvme_dd_direct_test_logs/analyze.sh
@@ -6,18 +6,18 @@ function report(){
 	for readwrite in 'Read' 'Write'; do
 		cmd="grep "$readwrite"\scmd\scalled $fn"
 		count=`$cmd -c`	
-		echo $readwrite" "$count
+		echo " * "$readwrite" "$count
 		for sqid in 1 2 3 4; do
 			cmd2="grep '"$readwrite"\scmd\scalled' "$fn" -A 4 -B 10 | grep '220:\ssq_id\s"$sqid"' -c"
 			#echo $cmd2
 			eval dastring=\`${cmd2}\`
-			echo -e '\t'"sqid $sqid $dastring"
+			echo -e '  * '"sqid $sqid $dastring"
 		done
 		for prp in PRP1 PRP2 prp_list; do
 			cmd2="grep '"$readwrite"\scmd\scalled' "$fn" -A 4 -B 10 | grep '$prp' -c"
 			#echo $cmd2
 			eval dastring=\`${cmd2}\`
-			echo -e '\t'"$prp $dastring"
+			echo -e '  * '"$prp $dastring"
 		done
 	done
 }
