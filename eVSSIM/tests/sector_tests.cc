c78a21d01324c810b165d697719df59a35a2ae85
datos <datos@datos-ThinkPad-T410.(none)>
2016-04-18 06:51:55 +0300

    Fixing ssd.conf parameters calculation based on disk size
    Reducing disk size was ac heaved by recalculating flash number.
    Which is wrong because this number can't drop too much.
    Block number should be is recalculated based on desired disk
    size.
    
    Change-Id: I651136efc47d25da150e7d7df96ea22ecc228e0d

--- eVSSIM/tests/sector_tests.cc
+++ eVSSIM/tests/sector_tests.cc
@@ -18,20 +18,32 @@ using namespace std;
 
 namespace {
 
+
+
     class SectorUnitTest : public ::testing::TestWithParam<size_t> {
         public:
+
+            //const static size_t CONST_BLOCK_NB_PER_FLASH = 4096;
+            const static size_t CONST_FLASH_NUMBER = 4;
+            const static size_t CONST_PAGES_PER_BLOCK = 8; // external (non over-provisioned)
+            const static size_t CONST_PAGES_PER_BLOCK_OVERPROV = (CONST_PAGES_PER_BLOCK * 25) / 100; // 25 % of pages for over-provisioning
+            const static size_t CONST_PAGE_SIZE_IN_BYTES = 4096;
+
+
             virtual void SetUp() {
                 size_t mb = GetParam();
-                pages_= mb * (1048576 / 4096); // number_of_pages = disk_size (in MB) * 1048576 / page_size
-                size_t block_x_flash = pages_ / 8; // all_blocks_on_all_flashes = number_of_pages / pages_in_block
-                size_t flash = block_x_flash / 4096; // number_of_flashes = all_blocks_on_all_flashes / number_of_blocks_in_flash
+                pages_= mb * ((1024 * 1024) / CONST_PAGE_SIZE_IN_BYTES); // number_of_pages = disk_size (in MB) * 1048576 / page_size
+                size_t block_x_flash = pages_ / CONST_PAGES_PER_BLOCK; // all_blocks_on_all_flashes = number_of_pages / pages_in_block
+                //size_t flash = block_x_flash / CONST_BLOCK_NB_PER_FLASH; // number_of_flashes = all_blocks_on_all_flashes / number_of_blocks_in_flash
+                size_t blocks_per_flash = block_x_flash / CONST_FLASH_NUMBER; // number_of_flashes = all_blocks_on_all_flashes / number_of_blocks_in_flash
+
                 ofstream ssd_conf("data/ssd.conf", ios_base::out | ios_base::trunc);
                 ssd_conf << "FILE_NAME ./data/ssd.img\n"
-                    "PAGE_SIZE 4096\n"
-                    "PAGE_NB 10\n" // 8 pages per block +2 pages over-provision = 125% of disk size
+                    "PAGE_SIZE " << CONST_PAGE_SIZE_IN_BYTES << "\n"
+                    "PAGE_NB " << (CONST_PAGES_PER_BLOCK + CONST_PAGES_PER_BLOCK_OVERPROV) << "\n"
                     "SECTOR_SIZE 1\n"
-                    "FLASH_NB " << flash << "\n" // see calculations above
-                    "BLOCK_NB 4096\n"
+                    "FLASH_NB " << CONST_FLASH_NUMBER << "\n"
+                    "BLOCK_NB " << blocks_per_flash << "\n"
                     "PLANES_PER_FLASH 1\n"
                     "REG_WRITE_DELAY 82\n"
                     "CELL_PROGRAM_DELAY 900\n"
@@ -40,7 +52,7 @@ namespace {
                     "BLOCK_ERASE_DELAY 2000\n"
                     "CHANNEL_SWITCH_DELAY_R 16\n"
                     "CHANNEL_SWITCH_DELAY_W 33\n"
-                    "CHANNEL_NB 4\n"
+                    "CHANNEL_NB " << CONST_FLASH_NUMBER << "\n"
                     "STAT_TYPE 15\n"
                     "STAT_SCOPE 62\n"
                     "STAT_PATH /tmp/stat.csv\n"
@@ -65,11 +77,13 @@ namespace {
             size_t pages_;
     }; // OccupySpaceStressTest
 
-    INSTANTIATE_TEST_CASE_P(DiskSize, SectorUnitTest, ::testing::Values(512 /*MB*/, 1024 /*1G*/, 4096 /*4G*/)); //Values are in MB
+
+	INSTANTIATE_TEST_CASE_P(DiskSize, SectorUnitTest, ::testing::Values(256/*MB*/, 256)); //, 1024 /*1G*/, 4096 /*4G*/)); //Values are in MB
 
     TEST_P(SectorUnitTest, SequentialOnePageAtTimeWrite) {
         for(int x=0; x<8; x++){
             for(size_t p=0; p < pages_; p++){
+                //std::cout << "hello"  << p << "\n";
                 ASSERT_EQ(SUCCESS, _FTL_WRITE_SECT(p * 4096, 1));
             }
         }
