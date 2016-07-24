21d3723760b02b20ccc5c51ce06dc68184d7813d
Chananel Engelberg <chananelyisrael@gmail.com>
2016-07-24 20:14:21 +0300

    Fixing object leaks.
    Change-Id: I09345ca8dcb3ab55659ee95c3da74c87a88246d4

--- eVSSIM/scripts/compile.sh
+++ eVSSIM/scripts/compile.sh
@@ -4,5 +4,5 @@ cd eVSSIM/MONITOR/SSD_MONITOR_PM
 qmake -o Makefile ssd_monitor_p.pro
 make
 cd ../../QEMU
-./configure --enable-io-thread --enable-linux-aio --target-list=x86_64-softmmu --enable-sdl --enable-vssim --extra-cflags='-Wno-deprecated-declarations -Wno-unused-but-set-variable'
+./configure --enable-io-thread --enable-linux-aio --target-list=x86_64-softmmu --enable-sdl --enable-vssim --extra-cflags='-Wno-error=deprecated-declarations -Wno-error=unused-but-set-variable'
 make
