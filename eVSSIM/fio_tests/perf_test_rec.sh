b0dddbb7ae4b87ab9b5bdd9499155f74f007059f
David Sariel <davidsa@openu.ac.il>
2016-08-26 15:30:04 +0300

    Removing fio_tests from eVSSIM folder
    The folder fio_tests was moved to tests/guest. This patch removes
    fio_tests from eVSSIM folder.
    
    Change-Id: I2031bb0baf0850f1dccbc548e82a8febd9df1950

--- eVSSIM/fio_tests/perf_test_rec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for tt in 'write' 'rw' 'read' ; do
-	echo "RECORDING REFERENCE RESULTS FOR $tt TEST"
-	./perf_test_rec_results.sh "test_"$tt
-done
-
-echo "ALL DONE"
-
--- eVSSIM/fio_tests/perf_test_run_short.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for tt in 'write' 'rw' 'read' ; do
-	echo "RUNNING $tt TEST"
-	./perf_test.sh "test_"$tt"_short"
-done
-
-echo "ALL DONE"
-
