874fcc1166dbd72f3c33750f853640211dd47f8d
Alexy T <alexy.trifonov@gmail.com>
2017-03-19 14:24:40 +0200

    Added support for intercommunication ( send , fetch )
    Change-Id: I65a7723c99a090b296764eb55629a269a6adf7d0

--- ThreadControl/ThreadControl.h
+++ ThreadControl/ThreadControl.h
@@ -2,25 +2,23 @@
  * ThreadControl.h
  *
  *  Created on: Mar 14, 2017
- *      Author: Alexy T.
+ *      Author: Alexy Trifonov.
  */
 
 #ifndef THREADCONTROL_H_
 #define THREADCONTROL_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h> /* superset of previous */
-#include <pthread.h>
-
 typedef int (*thread_execution_function)(void* pHandle );
 
-typedef enum { UPDATE_THREAD_INFO_TYPE_ENABLE , UPDATE_THREAD_INFO_TYPE_EXIT , UPDATE_THREAD_INFO_TYPE_LIB_PATH , UPDATE_THREAD_INFO_TYPE_ENTRY_POINT , UPDATE_THREAD_INFO_TYPE_SENTINEL } teUpdateThreadInfoType;
+typedef enum { UPDATE_THREAD_INFO_TYPE_ENABLE , UPDATE_THREAD_INFO_TYPE_EXIT , UPDATE_THREAD_INFO_TYPE_LIB_PATH , UPDATE_THREAD_INFO_TYPE_ENTRY_POINT , UPDATE_THREAD_INFO_TYPE_CUSTOM_DATA , UPDATE_THREAD_INFO_TYPE_SENTINEL } teUpdateThreadInfoType;
+
+typedef enum { ERROR_NONE , ERROR_INPUT_PARAM  = -1, ERROR_OS_RESOURCES  = -2, ERROR_OPEN_SOCKET  = -2} tsErrorList;
 
 int		ThreadControl_Init( unsigned int iNumberOfThreads , unsigned int* pvStackSizesList );
 int		ThreadControl_Update( unsigned int number , teUpdateThreadInfoType type , ... );
 void		ThreadControl_Close();
+
+int		SendMessage( void* pHandle , unsigned int uiThreadNumber , void *pData , unsigned int uiSize );
+int		FetchMessage( void* pHandle , unsigned int uiReadSize , void *pData , unsigned int uiDataSize );
 
 #endif /* THREADCONTROL_H_ */
