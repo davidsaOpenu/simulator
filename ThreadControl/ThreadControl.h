c87a05407bbfe7f51a31b947f1357087100c1c2b
Alexy T <alexy.trifonov@gmail.com>
2017-03-19 20:15:06 +0200

    1. Added Make File 2. Changed Tab Width 3. Repair FreeThreadInfoList Function
    Change-Id: Idf89cb58e911a0dbf4f711bfa0acf4d21f912b99

--- ThreadControl/ThreadControl.h
+++ ThreadControl/ThreadControl.h
@@ -2,7 +2,7 @@
  * ThreadControl.h
  *
  *  Created on: Mar 14, 2017
- *      Author: Alexy Trifonov.
+ *      Author: Alexey Trifonov.
  */
 
 #ifndef THREADCONTROL_H_
@@ -14,11 +14,11 @@ typedef enum { UPDATE_THREAD_INFO_TYPE_ENABLE , UPDATE_THREAD_INFO_TYPE_EXIT , U
 
 typedef enum { ERROR_NONE , ERROR_INPUT_PARAM  = -1, ERROR_OS_RESOURCES  = -2, ERROR_OPEN_SOCKET  = -2} tsErrorList;
 
-int		ThreadControl_Init( unsigned int iNumberOfThreads , unsigned int* pvStackSizesList );
-int		ThreadControl_Update( unsigned int number , teUpdateThreadInfoType type , ... );
+int			ThreadControl_Init( unsigned int iNumberOfThreads , unsigned int* pvStackSizesList );
+int			ThreadControl_Update( unsigned int number , teUpdateThreadInfoType type , ... );
 void		ThreadControl_Close();
 
-int		SendMessage( void* pHandle , unsigned int uiThreadNumber , void *pData , unsigned int uiSize );
-int		FetchMessage( void* pHandle , unsigned int uiReadSize , void *pData , unsigned int uiDataSize );
+int			SendMessage( void* pHandle , unsigned int uiThreadNumber , void *pData , unsigned int uiSize );
+int			FetchMessage( void* pHandle , unsigned int uiReadSize , void *pData , unsigned int uiDataSize );
 
 #endif /* THREADCONTROL_H_ */
