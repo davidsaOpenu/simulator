80418ce550e12d297ee76fb165108a715996f939
Nir Dothan <nir.dothan@gmail.com>
2017-06-25 00:07:56 +0300

    Combine mkfs.ext4 and nvme driver test
    a new test suite in pytest which makes an ext4 filesystem
    on the nvme device in the guest.
    It then preformes rmmod nvme, insmod nvme, mounts the fs and generates
    random files in various sizes on the mounted fs.
    The test suite continues with unmounting, rmmod nvme, insmod nvme,
    remounting and diffing file contents 10 times. Eventually it cleans
    up and unmounts the device.
    
    Change-Id: Id4be9fe9ab684b5a33903e9c6a377c01b6581dce

---
+++ eVSSIM/tests/guest/fs_tests/test_fs.py
@@ -0,0 +1,90 @@
+import pytest
+import os
+from subprocess import call
+import glob
+
+NVME_DEVICE='/dev/nvme0n1'
+MOUNTPOINT='/mnt/fstest'
+BACKUP_DIR='/home/esd'
+COMMON_DIR='/home/esd/guest/common'
+TEST_NUMBER_OF_ITERATIONS=10
+
+
+def rmmod():
+    assert 0==call(['rmmod','nvme'])
+
+def insmod():
+    assert 0==call(['modprobe','nvme'])
+
+def verify_dmesg(clear=False):
+    if clear:
+       return 1==call(COMMON_DIR+"/check-dmesg.sh -C", shell=True )
+    return 1==call(COMMON_DIR+"/check-dmesg.sh -l=warn,err,-s='ext4\|nvme'", shell=True )
+
+def sync_and_verify():
+    assert 0==call(['free'])
+    assert verify_dmesg()
+    assert 0==call(['sync'])
+    assert verify_dmesg()
+    assert 0==call("echo 3> /proc/sys/vm/drop_caches", shell=True)
+    assert verify_dmesg()
+    assert 0==call(['free'])
+    assert verify_dmesg()
+
+def mkfs():
+    assert 0==call(['mkfs.ext4',NVME_DEVICE])
+
+def mount():
+    if not os.path.exists(MOUNTPOINT):
+       assert 0==call(['mkdir',MOUNTPOINT])
+    assert 0==call(['mount', NVME_DEVICE, MOUNTPOINT])
+
+def generate_files():
+    for size in [2, 4, 8, 16, 32 ]:
+        mb="count={}".format(size)
+        targetfile="of={}/target-file_{}".format(MOUNTPOINT,size)
+        assert 0==call(['dd','if=/dev/urandom',targetfile,'bs=1M',mb])
+
+def backup_files():
+    assert 0==call("cp {}/target-file_* {}".format(MOUNTPOINT,BACKUP_DIR), shell=True )
+
+def diff_files():
+    for backupfile in glob.glob(BACKUP_DIR+"/target-file_*"):
+        targetfile=MOUNTPOINT+"/"+os.path.basename(backupfile)
+        assert 0==call(['diff', backupfile, targetfile])
+
+def umount():
+    assert 0==call(['umount',MOUNTPOINT])
+
+class TestFileSystem(object):
+
+    def test_setup(self):
+        call(['rmmod','nvme'])
+        verify_dmesg(clear=True)
+        insmod()
+        mkfs()
+        sync_and_verify()
+        mount()
+        sync_and_verify()
+        generate_files()
+        sync_and_verify()
+        backup_files()
+        sync_and_verify()
+        diff_files()
+
+    def test_iterations(self):
+        for _ in range(TEST_NUMBER_OF_ITERATIONS):
+          umount()
+          rmmod()
+          insmod()
+          sync_and_verify()
+          mount()
+          diff_files()
+
+
+    def test_cleanup(self):
+        assert 0==call("rm {}/target-file_*".format(MOUNTPOINT), shell=True )
+        assert 0==call("rm {}/target-file_*".format(BACKUP_DIR), shell=True )
+        umount()
+
+
