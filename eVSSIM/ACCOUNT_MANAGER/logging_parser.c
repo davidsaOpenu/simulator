adc4e89aa76cd34a260ec591d19a21419041b2f9
Yoav Kaempfer <yoav.kaempfer@gmail.com>
2017-08-08 12:39:38 +0300

    [WIP] Adding the infrastructure for an in-memory logging mechanism.
    Currently, delay is added to every operation in the simulator, as specified in ssd.conf.
    This patch adds the infrastructure for a new, in-memory logging mechanism, which
    will be used in the future to log the delays instead of actually add them.
    
    Change-Id: I26609c7c623d0b180182a2d03a65b36115885e09

---
+++ eVSSIM/ACCOUNT_MANAGER/logging_parser.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 The Open University of Israel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <math.h>
+#include <string.h>
+
+#include "logging_backend.h"
+#include "logging_parser.h"
+
+
+/*
+ * Log the structure given (as a byte array)
+ */
+void log_struct(Logger* logger, unsigned char* struct_buffer, int length, int struct_type) {
+	// write the type and then the structure itself
+	logger_write(logger, (logger_data_t*) &struct_type, 1);
+	logger_write(logger, (logger_data_t*) struct_buffer, ceil(length / (double) sizeof(logger_data_t)));
+}
+
+/*
+ * Return the type of the next log in the logger
+ */
+int next_log_type(Logger* logger) {
+	logger_data_t type;
+	logger_read(logger, &type, 1);
+	return (int) type;
+}
+
+/*
+ * Write the next log in the logger to the buffer
+ * It is up to the user to calculate `log_size` using the log type
+ */
+void next_log(Logger* logger, void* buffer, int log_size) {
+	// first, write the part of the log that is stored in full units of `logger_data_t`
+	int whole_effective_size = log_size / sizeof(logger_data_t);
+	int leftover_size = log_size - (whole_effective_size * sizeof(logger_data_t));
+	logger_read(logger, (logger_data_t*) buffer, whole_effective_size);
+	// if we have some more left, copy it manually
+	if (leftover_size > 0) {
+		logger_data_t leftover;
+		logger_read(logger, &leftover, 1);
+		memcpy(buffer + whole_effective_size * sizeof(logger_data_t), (void*) &leftover, leftover_size);
+	}
+}
