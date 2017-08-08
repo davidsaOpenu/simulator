ba7b21ad4e9fa6790667c9360dbc9248341709e7
Yoav Kaempfer <yoav.kaempfer@gmail.com>
2017-08-08 12:39:38 +0300

    [WIP] Adding the infrastructure for an in-memory logging mechanism.
    Currently, delay is added to every operation in the simulator, as specified in ssd.conf.
    This patch adds the infrastructure for a new, in-memory logging mechanism, which
    will be used in the future to log the delays instead of actually add them.
    
    Change-Id: I26609c7c623d0b180182a2d03a65b36115885e09

---
+++ eVSSIM/ACCOUNT_MANAGER/logging_backend.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2017 The Open University of Israel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * The backend logging mechanism is implemented using one long array in memory, which contains a circular array inside
+ * The array is aligned in memory, for a better caching performance
+ */
+
+#include "logging_backend.h"
