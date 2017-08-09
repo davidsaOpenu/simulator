317d090f3ea9eb496280abdfb7ddaf45f81604cb
David Sariel <davidsa@openu.ac.il>
2017-08-09 17:56:49 +0300

    Account manager test harness
    Change-Id: Iba061aab3da3563520608c5b1cd294b11b46d655

---
+++ eVSSIM/tests/host/account_mgr_tests.cc
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 The Open University of Israel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+extern "C" {
+
+#include "common.h"
+}
+bool g_ci_mode = false;
+
+#define GTEST_DONT_DEFINE_FAIL 1
+#include <gtest/gtest.h>
+
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "--ci") == 0) {
+            g_ci_mode = true;
+        }
+    }
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+using namespace std;
+
+namespace {
+
+} //namespace
