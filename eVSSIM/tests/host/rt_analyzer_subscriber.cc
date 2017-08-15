da9ca5504e22c236e5d6c556ef13bac060c7c42f
Yoav Kaempfer <yoav.kaempfer@gmail.com>
2017-08-15 15:05:47 +0300

    Integrating the monitor with the new logger
    Currently, The monitor uses sockets in order to get the logs from
    the simulator.
    This patch changes the monitor so that it will support the new
    logging mechanism implemented in eVSSIM/LOG_MGR, via the code in
    monitor_test.h/cpp.
    This patch also adds all the needed log types for the analyzer.
    
    Change-Id: I0d0830eee9ba5a44d4482dc0d4c0048fbc01a1ac

--- eVSSIM/tests/host/rt_analyzer_subscriber.cc
+++ eVSSIM/tests/host/rt_analyzer_subscriber.cc
@@ -31,24 +31,174 @@ extern "C" {
 using namespace std;
 
 namespace rt_subscriber {
+    // delay constants as written to ssd.conf by log_mgr_tests.cc
+    const static int REG_WRITE_DELAY = 82;
+    const static int CELL_PROGRAM_DELAY = 900;
+    const static int REG_READ_DELAY = 82;
+    const static int CELL_READ_DELAY = 50;
+    const static int BLOCK_ERASE_DELAY = 2000;
+    const static int CHANNEL_SWITCH_DELAY_R = 16;
+    const static int CHANNEL_SWITCH_DELAY_W = 33;
+
+    // some more constants
+    const static int SEC_IN_USEC = 1000000;
+    const static int PAGES_IN_SSD = 2 * 8 * 4;
+
     int _logs_read = 0;
     RTLogAnalyzer* _analyzer = NULL;
+    SSDStatistics results[] = {
+            // register read
+            {
+                    .write_count = 0,
+                    .write_speed = 0,
+                    .read_count = 0,
+                    .read_speed = 0,
+                    .garbage_collection_count = 0,
+                    .write_amplification = 0,
+                    .utilization = 0
+            },
+            // physical cell read
+            {
+                    .write_count = 0,
+                    .write_speed = 0,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 0,
+                    .write_amplification = 0,
+                    .utilization = 0
+            },
+            // channel switch to write
+            {
+                    .write_count = 0,
+                    .write_speed = 0,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 0,
+                    .write_amplification = 0,
+                    .utilization = 0
+            },
+            // physical cell program
+            {
+                    .write_count = 1,
+                    .write_speed = (1.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 0,
+                    .write_amplification = 0,
+                    .utilization = 1.0 / PAGES_IN_SSD
+            },
+            // garbage collection
+            {
+                    .write_count = 1,
+                    .write_speed = (1.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 0,
+                    .utilization = 1.0 / PAGES_IN_SSD
+            },
+            // logical cell program
+            {
+                    .write_count = 1,
+                    .write_speed = (1.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 1,
+                    .utilization = 1.0 / PAGES_IN_SSD
+            },
+            // register write
+            {
+                    .write_count = 1,
+                    .write_speed = (1.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 1,
+                    .utilization = 1.0 / PAGES_IN_SSD
+            },
+            // physical cell program
+            {
+                    .write_count = 2,
+                    .write_speed = (2.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY + REG_WRITE_DELAY + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 2.0,
+                    .utilization = 2.0 / PAGES_IN_SSD
+            },
+            // logical cell program
+            {
+                    .write_count = 2,
+                    .write_speed = (2.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY + REG_WRITE_DELAY + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 1.0,
+                    .utilization = 2.0 / PAGES_IN_SSD
+            },
+            // block erase
+            {
+                    .write_count = 2,
+                    .write_speed = (2.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY + REG_WRITE_DELAY + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 1.0,
+                    .utilization = 0.0 / PAGES_IN_SSD
+            },
+            // channel switch to read
+            {
+                    .write_count = 2,
+                    .write_speed = (2.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY + REG_WRITE_DELAY + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 1,
+                    .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 1.0,
+                    .utilization = 0.0 / PAGES_IN_SSD
+            },
+            // physical cell read
+            {
+                    .write_count = 2,
+                    .write_speed = (2.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY + REG_WRITE_DELAY + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 2,
+                    .read_speed = (2.0 / (REG_READ_DELAY + CELL_READ_DELAY + BLOCK_ERASE_DELAY + CHANNEL_SWITCH_DELAY_R + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 1,
+                    .write_amplification = 1.0,
+                    .utilization = 0.0 / PAGES_IN_SSD
+            },
+            // garbage collection
+            {
+                    .write_count = 2,
+                    .write_speed = (2.0 / (CHANNEL_SWITCH_DELAY_W + CELL_PROGRAM_DELAY + REG_WRITE_DELAY + CELL_PROGRAM_DELAY)) * SEC_IN_USEC,
+                    .read_count = 2,
+                    .read_speed = (2.0 / (REG_READ_DELAY + CELL_READ_DELAY + BLOCK_ERASE_DELAY + CHANNEL_SWITCH_DELAY_R + CELL_READ_DELAY)) * SEC_IN_USEC,
+                    .garbage_collection_count = 2,
+                    .write_amplification = 1.0,
+                    .utilization = 0.0 / PAGES_IN_SSD
+            }
+    };
 
     void write() {
         Logger* logger = _analyzer->logger;
         GarbageCollectionLog GC_LOG;
-        LOG_PHYSICAL_PAGE_READ(logger, (PhysicalPageReadLog) { .channel = 1, .block = 2, .page = 3});
-        LOG_PHYSICAL_PAGE_WRITE(logger, (PhysicalPageWriteLog) { .channel = 4, .block = 5, .page = 6});
+        LOG_REGISTER_READ(logger, (RegisterReadLog) { .channel = 19, .die = 20, .reg = 21});
+        LOG_PHYSICAL_CELL_READ(logger, (PhysicalCellReadLog) { .channel = 1, .block = 2, .page = 3});
+        LOG_CHANNEL_SWITCH_TO_WRITE(logger, (ChannelSwitchToWriteLog) { .channel = 28 });
+        LOG_PHYSICAL_CELL_PROGRAM(logger, (PhysicalCellProgramLog) { .channel = 4, .block = 5, .page = 6});
         LOG_GARBAGE_COLLECTION(logger, GC_LOG);
-        LOG_LOGICAL_PAGE_WRITE(logger, (LogicalPageWriteLog) { .channel = 7, .block = 8, .page = 9});
-        LOG_PHYSICAL_PAGE_WRITE(logger, (PhysicalPageWriteLog) { .channel = 10, .block = 11, .page = 12});
-        LOG_LOGICAL_PAGE_WRITE(logger, (LogicalPageWriteLog) { .channel = 13, .block = 14, .page = 15});
-        LOG_PHYSICAL_PAGE_READ(logger, (PhysicalPageReadLog) { .channel = 16, .block = 17, .page = 18});
+        LOG_LOGICAL_CELL_PROGRAM(logger, (LogicalCellProgramLog) { .channel = 7, .block = 8, .page = 9});
+        LOG_REGISTER_WRITE(logger, (RegisterWriteLog) {.channel = 22, .die = 23, .reg = 24});
+        LOG_PHYSICAL_CELL_PROGRAM(logger, (PhysicalCellProgramLog) { .channel = 10, .block = 11, .page = 12});
+        LOG_LOGICAL_CELL_PROGRAM(logger, (LogicalCellProgramLog) { .channel = 13, .block = 14, .page = 15});
+        LOG_BLOCK_ERASE(logger, (BlockEraseLog) {.channel = 25, .die = 26, .block = 27});
+        LOG_CHANNEL_SWITCH_TO_READ(logger, (ChannelSwitchToReadLog) { .channel = 29 });
+        LOG_PHYSICAL_CELL_READ(logger, (PhysicalCellReadLog) { .channel = 16, .block = 17, .page = 18});
         LOG_GARBAGE_COLLECTION(logger, GC_LOG);
     }
 
     void read() {
-        rt_log_analyzer_loop(_analyzer, 8);
+        rt_log_analyzer_loop(_analyzer, 13);
     }
 
     /**
@@ -56,66 +206,16 @@ namespace rt_subscriber {
      * @param stats the statistics to validate
      */
     void analyze_stats(SSDStatistics stats) {
+        SSDStatistics res = results[_logs_read];
         _logs_read++;
 
-        switch (_logs_read) {
-            case 1:
-                // physical page read
-                ASSERT_EQ(0, stats.write_count);
-                ASSERT_EQ(1, stats.read_count);
-                ASSERT_EQ(0, stats.garbage_collection_count);
-                ASSERT_EQ(0.0, stats.write_amplification);
-                break;
-            case 2:
-                // physical page write
-                ASSERT_EQ(1, stats.write_count);
-                ASSERT_EQ(1, stats.read_count);
-                ASSERT_EQ(0, stats.garbage_collection_count);
-                ASSERT_EQ(0.0, stats.write_amplification);
-                break;
-            case 3:
-                // garbage collection
-                ASSERT_EQ(1, stats.write_count);
-                ASSERT_EQ(1, stats.read_count);
-                ASSERT_EQ(1, stats.garbage_collection_count);
-                ASSERT_EQ(0.0, stats.write_amplification);
-                break;
-            case 4:
-                // logical page write
-                ASSERT_EQ(1, stats.write_count);
-                ASSERT_EQ(1, stats.read_count);
-                ASSERT_EQ(1, stats.garbage_collection_count);
-                ASSERT_EQ(1.0, stats.write_amplification);
-                break;
-            case 5:
-                // physical page write
-                ASSERT_EQ(2, stats.write_count);
-                ASSERT_EQ(1, stats.read_count);
-                ASSERT_EQ(1, stats.garbage_collection_count);
-                ASSERT_EQ(2.0, stats.write_amplification);
-                break;
-            case 6:
-                // logical page write
-                ASSERT_EQ(2, stats.write_count);
-                ASSERT_EQ(1, stats.read_count);
-                ASSERT_EQ(1, stats.garbage_collection_count);
-                ASSERT_EQ(1.0, stats.write_amplification);
-                break;
-            case 7:
-                // physical page read
-                ASSERT_EQ(2, stats.write_count);
-                ASSERT_EQ(2, stats.read_count);
-                ASSERT_EQ(1, stats.garbage_collection_count);
-                ASSERT_EQ(1.0, stats.write_amplification);
-                break;
-            case 8:
-                // garbage collection
-                ASSERT_EQ(2, stats.write_count);
-                ASSERT_EQ(2, stats.read_count);
-                ASSERT_EQ(2, stats.garbage_collection_count);
-                ASSERT_EQ(1.0, stats.write_amplification);
-                break;
-        }
+        ASSERT_EQ(res.write_count, stats.write_count);
+        ASSERT_EQ(res.write_speed, stats.write_speed);
+        ASSERT_EQ(res.read_count, stats.read_count);
+        ASSERT_EQ(res.read_speed, stats.read_speed);
+        ASSERT_EQ(res.garbage_collection_count, stats.garbage_collection_count);
+        ASSERT_EQ(res.write_amplification, stats.write_amplification);
+        ASSERT_EQ(res.utilization, stats.utilization);
     }
 
     void subscribe(RTLogAnalyzer* analyzer) {
