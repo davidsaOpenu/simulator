a86245b17fe871878e0991ad12afd0178a35c3bf
Yoav Kaempfer <yoav.kaempfer@gmail.com>
2017-08-22 14:09:29 +0300

    Full integration of the logging mechanism
    Full integration of the new logging mechanism to the simulator.
    There is a different logger for each flash in the simulator, a real
    time analyzer for each logger, one manager and one server.
    
    Change-Id: I967bde03cdf0e852fc3903d751be6e55168429fd

---
+++ eVSSIM/LOG_MGR/logging_statistics.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2017 The Open University of Israel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+
+#include "logging_statistics.h"
+
+
+SSDStatistics stats_init(void) {
+    SSDStatistics stats = {
+            .write_count = 0,
+            .write_speed = 0.0,
+            .read_count = 0,
+            .read_speed = 0.0,
+            .garbage_collection_count = 0,
+            .write_amplification = 0.0,
+            .utilization = 0.0
+    };
+    return stats;
+}
+
+
+int stats_json(SSDStatistics stats, Byte* buffer, int max_len) {
+    return snprintf((char*) buffer, max_len, "{"
+                    "\"write_count\":%d,"
+                    "\"write_speed\":%f,"
+                    "\"read_count\":%d,"
+                    "\"read_speed\":%f,"
+                    "\"garbage_collection_count\":%d,"
+                    "\"write_amplification\":%f,"
+                    "\"utilization\":%f"
+                    "}",
+                    stats.write_count, stats.write_speed, stats.read_count,
+                    stats.read_speed, stats.garbage_collection_count,
+                    stats.write_amplification, stats.utilization);
+}
+
+
+int stats_equal(SSDStatistics first, SSDStatistics second) {
+    return first.write_count == second.write_count &&
+           first.write_speed == second.write_speed &&
+           first.read_count == second.read_count &&
+           first.read_speed == second.read_speed &&
+           first.garbage_collection_count == second.garbage_collection_count &&
+           first.write_amplification == second.write_amplification &&
+           first.utilization == second.utilization;
+}
