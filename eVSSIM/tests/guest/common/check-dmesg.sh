7f230243d41d5637649b16ac335b844f5aed6b39
Nir Dothan <nir.dothan@gmail.com>
2017-08-29 19:24:25 +0300

    Create script to grep dmesg
    Answer a common project need to check specific kernel errors
    in dmesg log.
    Create a new script to be used by multiple guest tests.
    Script usage: check-dmesg.sh -l=[log level (default:err)] -s=[searched string]
                  or to clear dmesg ring buffer: check-dmesg.sh -C
    returns 0 if string is found and 1 if not
    example:  check-dmesg.sh -l=warn,err -s="cgroup\|BIOS"
    
    Change-Id: I2800619961dfb45e154abe2304f780f5bb0c885a

---
+++ eVSSIM/tests/guest/common/check-dmesg.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+for i in "$@"
+do
+case $i in
+    -s=*|--string=*)
+    STRING="${i#*=}"
+    shift # past argument=value
+    ;;
+    -l=*|--loglevel=*)
+    LEVEL="${i#*=}"
+    shift # past argument=value
+    ;;
+    -C|--clear)  
+    echo " "
+    echo "Clearing dmesg ring buffer "
+    dmesg -C
+    echo " "
+    exit 
+    ;;
+    *)
+    echo " "
+    echo " "
+    echo "usage:   $0 -l=[log level (default:err)] -s=[searched string] "
+    echo "         $0 -C"
+    echo " "
+    echo "         returns 0 if found or 1 if not found"
+    echo " "
+    echo 'example:  $0 -l=warn,err -s="cgroup\|BIOS"'
+    echo " "
+    echo " "
+    exit -1
+            
+    ;;
+esac
+done
+
+dmesg -x --level=${LEVEL:-err} | grep ${STRING}
