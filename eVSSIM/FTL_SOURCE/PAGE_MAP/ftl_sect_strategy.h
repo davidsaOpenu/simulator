15b6090c77cb7a830bba7a11e8b8e1443aef9520
Nir Dothan <nir.dothan@gmail.com>
2017-08-30 12:45:52 +0300

    Squashing 4 commits of Nir Dothan and Hen Keselman
    Embed osd inside FTL and partition by nsid
    
    Make FTL_INIT and FTL_TERM APIs namespaced
    Prepare to maintain osd, per namespace, inside the FTL module.
    Modify calls from nvme_storage to FTL_INIT and FTL_TERM to occur
    per namespace.
    Move the existing FTL_TERM call from vl.c to nvme_storage.c
    hoever - we must make sure that it gets called.
    
    Change-Id: Ic5bfeb11509f8dc5b100d1582877c75841048295
    -------------------------------------------------------------------------------
    
    Encapsulated osd_* funcs inside _FTL_OBJ_* funcs
    
    Before this commit: osd functions were called explicitly in object_test.cc
    after calling to the matching _FTL_OBJ_* function.
    
    Currently: osd functions are called from inside the _FTL_OBJ_*
    functions.
    -------------------------------------------------------------------------------
    
    Added OSD_PERSISTENT and converted tabs to spaces
    
    Fixed CR: Added OSD_PERSISTENT configuration option to ssd.conf in order to
    control enablement of osd (inside FTL functions) and also converted tabs to
    spaces.
    -------------------------------------------------------------------------------
    
    Fixing SIGSEGV, on top of OSD embedment in FTL
    
    Fixing SIGSEGV with a patch provided by Nir Dothan, on top of previous
    commits that encapsulated OSD functions inside FTL_OBJ functions.
    
    Change-Id: Ie33e02c37584cafd93fbd703a026984230f2f9c0

--- eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_sect_strategy.h
+++ eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_sect_strategy.h
@@ -5,6 +5,8 @@
 
 ftl_ret_val _FTL_READ_SECT(uint64_t sector_nb, unsigned int length);
 ftl_ret_val _FTL_WRITE_SECT(uint64_t sector_nb, unsigned int length);
+void _FTL_SECT_STRATEGY_INIT(partition_id_t part_id, psize_t size);
+void _FTL_SECT_STRATEGY_TERM(partition_id_t part_id);
 
 ftl_ret_val _FTL_READ(uint64_t sector_nb, unsigned int offset, unsigned int length);
 ftl_ret_val _FTL_WRITE(uint64_t sector_nb, unsigned int offset, unsigned int length);
