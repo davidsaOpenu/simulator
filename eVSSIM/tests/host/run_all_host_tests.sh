bb3a1d0efe1a1c920d2c5bc74543fce16a6657d8
David Sariel <davidsa@openu.ac.il>
2017-10-03 03:24:12 +0300

    [WIP] Reduce test cycle run-time
    Change-Id: I241bf3190393f2c7da6d8530ed73289255bb43ad

--- eVSSIM/tests/host/run_all_host_tests.sh
+++ eVSSIM/tests/host/run_all_host_tests.sh
@@ -8,11 +8,11 @@ valgrind --leak-check=full --error-exitcode=2 ./sector_tests --ci
 CURR_RESULT=$?
 
 # run object tests
-valgrind --leak-check=full --error-exitcode=2 ./object_tests
-CURR_RESULT=$?
-if [ $CURR_RESULT -ne 0 ]; then
-  RESULT=1
-fi
+#valgrind --leak-check=full --error-exitcode=2 ./object_tests
+#CURR_RESULT=$?
+#if [ $CURR_RESULT -ne 0 ]; then
+#  RESULT=1
+#fi
 
 # run log manager tests
 valgrind --leak-check=full --error-exitcode=2 ./log_mgr_tests
