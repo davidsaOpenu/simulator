46d5440373744e71a2cec29c515eff79ad3ff111
David <davidlichterov@gmail.com>
2018-01-01 16:02:24 +0200

    Implements: logging buffer pools
    Currently the logging backend uses 16M logging buffer.
    The new logging backend mechanism implements logging buffer pool of size N,
    Where each logging pool is of 1/2M.

--- eVSSIM/LOG_MGR/logging_backend.c
+++ eVSSIM/LOG_MGR/logging_backend.c
@@ -20,85 +20,114 @@
 
 #include "logging_backend.h"
 
-
+//TODO - check if this is still needed
 /**
  * Return the size of the logger (full slots only)
  * @param {Logger*} logger the logger to check
  * @return the size of the logger (in bytes)
  */
-#define LOGGER_SIZE(logger) (((logger->head - logger->tail) + logger->buffer_size) % \
-                             logger->buffer_size)
+#define LOGGER_NODE_SIZE(logger) (((logger->head - logger->tail) + logger->buffer_size) % \
+		logger->buffer_size)
 
 
-Logger* logger_init(size_t size) {
-    // allocate the buffer such that it is aligned to the cache line
-    Byte* buffer;
-    if (posix_memalign((void**) &buffer, LOGGER_BUFFER_ALIGNMENT, size))
-        return NULL;
+Logger_Pool* logger_init(unsigned int number_of_nodes) {
+	Byte* buffer;
+	Logger_Pool_Node* head,tail,cur,priv;
 
-    // try to allocate the logger struct
-    Logger* logger = (Logger*) malloc(sizeof(Logger));
-    if (logger == NULL) {
-        free((void*) buffer);
-        return NULL;
-    }
+	// try to allocate number_of_nodes logger pool nodes
+	for(int i=0; i<number_of_nodes; i++) {
+		Logger_Pool_Node* logger_pool_node = (Logger_Pool_Node*) malloc(sizeof(Logger_Pool_Node));
+		if(logger_pool_node == NULL)
+			return NULL;
 
-    // update the fields and return the newly created logger
-    logger->head = logger->tail = logger->buffer = buffer;
-    logger->buffer_size = size;
-    return logger;
+		if (posix_memalign((void**) &buffer, LOGGER_BUFFER_ALIGNMENT, LOGGER_BUFFER_POOL_SIZE))
+			return NULL;
+
+		// if this is the first node allocation set correctly the Logger_Pool_Node structre
+		// and save it as the head node
+		if(i==0){
+			head = tail = cur = priv = logger_pool_node;
+			cur->priv = NULL;
+			cur->next = NULL;
+			cur->head = cur->tail = cur->buffer = buffer;
+			continue;
+		}
+
+		priv = cur;
+		priv->next = cur =  logger_pool_node;
+		cur->priv = priv;
+		cur->next = NULL;
+		cur->head = cur-> tail = cur->buffer = buffer;
+
+		// if this is the last node allocation save it as the tail node
+		if(i == (number_of_areas-1))
+			tail = cur;
+	}
+
+	// allocate Logger_Pool structre to hold and mange the pool of nodes
+	// that were allocated above
+	Logger_Pool* logger_pool = (Logger_Pool*) malloc(sizeof(Logger_Pool));
+	if(logger_pool == NULL)
+		return NULL;
+
+	logger_pool->head_pool_node = head;
+	logger_pool->tail_pool_node = tail;
+	logger_pool->free_pool_node = head;
+	logger_pool->number_of_free_pool_nodes = number_of_nodes;
+
+	return logger_pool;
 }
 
-int logger_write(Logger* logger, Byte* buffer, int length) {
-    int res = 0;
-    // print a warning if the buffer is not empty enough (one empty byte indicates a full buffer)
-    if (logger->buffer_size - LOGGER_SIZE(logger) - 1 < length) {
-        fprintf(stderr, "WARNING: the logger's buffer may have an overflow!\n");
-        fprintf(stderr, "WARNING: the log may now be corrupted and unusable!\n");
-        res = 1;
-    }
+int logger_write(Logger_Pool* logger_pool, Byte* buffer, int length) {
+	Byte* mem_buf;
+	int res=0, number_of_needed_pool_nodes=0;
 
-    // first, copy data until the end of the buffer of the logger
-    int first_part_length = length;
-    Byte* new_head = logger->head + length;
-    if ((logger->head - logger->buffer) + length >= logger->buffer_size) {
-        first_part_length = logger->buffer_size - (logger->head - logger->buffer);
-        new_head = logger->buffer;
-    }
-    memcpy((void*) logger->head, (void*) buffer, first_part_length);
-    logger->head = new_head;
+	// calculate how many logger pool nodes we need to write this data
+	number_of_needed_pool_nodes = (length / LOGGER_BUFFER_POOL_SIZE)+1;
 
-    // next, copy the rest of the data (if any left)
-    memcpy((void*) logger->head, (void*) (buffer + first_part_length), length - first_part_length);
-    logger->head += length - first_part_length;
+	// TODO
+	// check if there is a free node to write to
+	// or if the amount of data that is going to be wrriten is bigger then
+	// LOGGER_BUFFER_POOL_SIZE
+	// we need to handle  a case where we start to write to one pool and then need to swap to
+	// other pool
+	if(logger_pool->number_of_free_pool_nodes < number_of_needed_pool_nodes) {
+		// lets allocate number_of_needed_pool_nodes
+		for(int i=0; i<number_of_needed_pool_nodes; i++)
+		{
+			Logger_Pool_Node* logger_pool_node = (Logger_Pool_Node*) malloc(sizeof(Logger_Pool_Node));
+			if(logger_pool_node == NULL)
+				return 1;
 
-    return res;
+			if (posix_memalign((void**) &mem_buf, LOGGER_BUFFER_ALIGNMENT, LOGGER_BUFFER_POOL_SIZE))
+				return 1;
+
+			logger_pool_node->head = logger_pool_node->tail = logger_pool_node->buffer = mem_buf;
+			logger_pool_node->next = NULL;
+			logger_pool_node->priv = logger_pool->tail_pool_node;
+
+			logger_pool->tail = logger_pool_node;
+			logger_pool->number_of_free_pool_nodes++;
+			if(i == 0)
+				logger_pool->free_pool_node = logger_pool_node;
+		}
+	}
+
+	if(!((logger_pool->free_pool_node->head - logger_pool->free_pool_node->buffer) + length >= LOGGER_BUFFER_POOL_SIZE))
+	{
+		memcpy((void*) logger_pool->free_pool_node->head, (void*) buffer, length);
+		logger_pool->free_pool_node->head = logger_pool->free_pool_node->head + length;
+	}
+	// TODO - if the condition is not ture
+
+	return res;
 }
 
-int logger_read(Logger* logger, Byte* buffer, int length) {
-    // crop the maximum length
-    int logger_size = LOGGER_SIZE(logger);
-    int length_read = logger_size < length ? logger_size : length;
-
-    // first, copy data until the end of the buffer of the logger
-    int first_part_length = length_read;
-    Byte* new_tail = logger->tail + length_read;
-    if ((logger->tail - logger->buffer) + length_read >= logger->buffer_size) {
-        first_part_length = logger->buffer_size - (logger->tail - logger->buffer);
-        new_tail = logger->buffer;
-    }
-    memcpy((void*) buffer, (void*) logger->tail, first_part_length);
-    logger->tail = new_tail;
-
-    // next, copy the rest of the data (if any left)
-    memcpy((void*) (buffer + first_part_length), (void*) logger->tail,
-           length_read - first_part_length);
-    logger->tail += length_read - first_part_length;
-
-    return length_read;
+int logger_read(Logger_Pool* logger_pool, Byte* buffer, int length) {
+	// TODO
 }
+
 
 void logger_free(Logger* logger) {
-    free((void*) logger->buffer);
-    free((void*) logger);
+	// TODO
 }
--- eVSSIM/LOG_MGR/logging_backend.h
+++ eVSSIM/LOG_MGR/logging_backend.h
@@ -20,7 +20,7 @@
 
 #include <stdlib.h>
 
-
+// TODO - update this explantion
 /*
  * The backend logging mechanism is implemented using one long array in memory, which contains a
  * circular array inside; The array is aligned in memory, for a better caching performance
@@ -33,52 +33,84 @@
  */
 #define LOGGER_BUFFER_ALIGNMENT 64
 
+// TODO - understand if this is needed here or we can use the define that allready exists (1 << 24)
+// 0.5 mega byte logging area pool's
+#define LOGGER_BUFFER_POOL_SIZE (1 << 19)
 
 /**
  * A byte typedef, for easier use
  */
 typedef unsigned char Byte;
 
-
 /**
- * The logger structure
+ * The Logger Pool Node structure
+ * Each node represents a place in the memmory pool
+ * Where the logger may write/read from.
  */
 typedef struct {
-    /**
-     * The main buffer of the logger
-     */
-    Byte* buffer;
-    /**
-     * The next place to insert a byte at the buffer
-     */
-    Byte* head;
-    /**
-     * The next place to read a byte from the buffer
-     */
-    Byte* tail;
-    /**
-     * The size of the buffer (including empty slots)
-     */
-    size_t buffer_size;
-} Logger;
+	/**
+	 * The main buffer of the logger node
+	 */
+	Byte* buffer;
+	/**
+	 * The next place to insert a byte at the buffer
+	 */
+	Byte* head;
+	/**
+	 * The next place to read a byte from the buffer
+	 */
+	Byte* tail;
+	/**
+	 * The next pool node
+	 */
+	Logger_Pool_Node* next;
+	/**
+	 * The previous pool node
+	 */
+	Logger_Pool_Node* priv;
+} Logger_Pool_Node;
+
+/**
+ * The Logger Pool structure
+ * Each logger pool holds number_of_ree_pool_nodes
+ * That may be allocated for the looger to use
+ */
+typedef struct {
+	/**
+	 * Head node of Logger pool
+	 */
+	Logger_Pool_Node* head_pool_node;
+	/**
+	 * Tail node of Logger pool
+	 */
+	Logger_Pool_Node* tail_pool_node;
+	/**
+	 * Next free node of Logger pool
+	 */
+	Logger_Pool_Node* free_pool_node;
+	/**
+	 * Number of total free pool nodes in the Logger pool
+	 */
+	unsigned int number_of_free_pool_nodes;
+} Logger_Pool;
 
 /**
  * Create a new logger
- * @param size the size of the buffer to allocate for the logger
+ * @param number_of_nodes the number of nodes to allocate at this logger pool
  *  Due to the way posix_memalign works, size must be a power of two multiple of sizeof(void *)
  * @return the newly created logger
  */
-Logger* logger_init(size_t size);
+Logger_Pool* logger_init(unsigned int number_of_nodes);
 
 /**
  * Write a byte array to the logger
  * Print a warning to stderr if the logger cannot fit all the data
- * @param logger the logger to write the bytes to
+ * @param logger_pool struct that holds the logger node to write the bytes to
  * @param buffer the buffer to read the bytes from
  * @param length the number of bytes to copy from the buffer to the logger
  * @return 0 if the logger can fit all the data, otherwise nonzero
  */
-int logger_write(Logger* logger, Byte* buffer, int length);
+int logger_write(Logger_Pool* logger_pool, Byte* buffer, int length);
 
 /**
  * Read a byte array from the logger
@@ -87,13 +119,12 @@ int logger_write(Logger* logger, Byte* buffer, int length);
  * @param length the maximum number of bytes to read
  * @return the number of bytes read
  */
-int logger_read(Logger* logger, Byte* buffer, int length);
+int logger_read(Logger_Pool* logger_pool, Byte* buffer, int length);
 
 /**
  * Free the logger
  * @param logger the logger to free
  */
 void logger_free(Logger* logger);
-
 
 #endif
