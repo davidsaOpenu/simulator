20e1ba1be06bd5f38d787dd700aec88a94feefbd
Shimi Gersner <gersner@gmail.com>
2018-04-29 01:20:22 +0300

    Implements: Next-Gen Build Environment - Unified environment for both development & ci. - Uses docker and set of simplified bash scripts to manage build process. - Aims for <30m from initial checkout to working setup. - Allow very fast development times for QEMU and Guest Kernel code. - Compatibility with latest qemu release
    Change-Id: Ida4fc84ff0d58d34775a7fae1eb8a44dcb12a2c4
    Ticket: https://trello.com/c/aCsmX4bd

---
+++ infra/builder/run-ci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Configure a unique image name for the test
+export EVSSIM_DOCKER_IMAGE_NAME=evssim:ci-$(date "+%Y.%m.%d-%H.%M.%S")
+
+# Make sure every run we get a new clear runtime folder
+export EVSSIM_RUNTIME_ALWAYS_RESET=yes
+
+# Build images
+./build-docker-image.sh
+./build-qemu-image.sh
+
+# Compile all modules
+./compile-kernel.sh
+./compile-qemu.sh
+./compile-monitor.sh
+./compile-nvme-tools.sh
+./compile-tests.sh
+
+# Run sanity and tests
+./docker-run-sanity.sh
+./docker-test-host.sh
+./docker-test-guest.sh
