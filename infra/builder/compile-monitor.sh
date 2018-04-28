f9b3619ee8aa7fe62ba24320ef15f896ce7d7fc0
Shimi Gersner <gersner@gmail.com>
2018-04-29 01:20:22 +0300

    Implements: Next-Gen Build Environment - Unified environment for both development & ci. - Uses docker and set of simplified bash scripts to manage build process. - Aims for <30m from initial checkout to working setup. - Allow very fast development times for QEMU and Guest Kernel code.
    Change-Id: Ida4fc84ff0d58d34775a7fae1eb8a44dcb12a2c4
    Ticket: https://trello.com/c/aCsmX4bd

---
+++ infra/builder/compile-monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source ./builder.sh
+
+evssim_run_at_path $EVSSIM_SIMULATOR_FOLDER/eVSSIM/MONITOR/SSD_MONITOR_PM "qmake -o Makefile ssd_monitor_p.pro && make"
+
+# NOTE Broken code
+#evssim_run_at_path $EVSSIM_SIMULATOR_FOLDER/eVSSIM/osc-osd "make target_clean && make target"
