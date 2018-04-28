d8657931f44c4d7c3632c34fb1c5875c067f760e
Shimi Gersner <gersner@gmail.com>
2018-04-29 01:20:22 +0300

    Implements: Next-Gen Build Environment - Unified environment for both development & ci. - Uses docker and set of simplified bash scripts to manage build process. - Aims for <30m from initial checkout to working setup. - Allow very fast development times for QEMU and Guest Kernel code. - Compatibility with latest qemu release
    Change-Id: Ida4fc84ff0d58d34775a7fae1eb8a44dcb12a2c4
    Ticket: https://trello.com/c/aCsmX4bd

--- eVSSIM/tests/guest/nvme_compliance_tests/test_nvme_compliance.py
+++ eVSSIM/tests/guest/nvme_compliance_tests/test_nvme_compliance.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import os
+import time
+import pytest
+import subprocess
+
+VSSIM_NEXTGEN_BUILD_SYSTEM = "VSSIM_NEXTGEN_BUILD_SYSTEM" in os.environ
+
+old_qemu_only = pytest.mark.skipif(VSSIM_NEXTGEN_BUILD_SYSTEM, reason="Old qemu only")
+new_qemu_only = pytest.mark.skipif(not VSSIM_NEXTGEN_BUILD_SYSTEM, reason="New qemu only")
 
 NVME_COMPLIANCE_TEST_DIR = "/home/esd/nvmeCompl/"
 TEST_DIR = "/home/esd/guest/nvme_compliance_tests"
 
 class TestNVMeCompliance:
-    def test_NVMeCompliance(self):
+    @old_qemu_only
+    def test_NVMeCompliance_old(self):
         os.system("rmmod nvme")
 
         os.chdir(NVME_COMPLIANCE_TEST_DIR + "dnvme")
@@ -17,6 +26,23 @@ class TestNVMeCompliance:
         skipSuites = [ 16, 17]
         for suiteNum in range(0, 24):
             if suiteNum not in skipSuites:
-               cmd = "./tnvme --test=%d > test%d.txt 2>&1" % (suiteNum, suiteNum)
-               print cmd
-               assert 0 == os.system(cmd)
+                cmd = "./tnvme --test=%d > test%d.txt 2>&1" % (suiteNum, suiteNum)
+                print cmd
+                assert 0 == os.system(cmd)
+
+    @new_qemu_only
+    def test_NVMeCompliance_new(self):
+        if 0 != subprocess.call("lsmod | grep dnvme",
+                                shell=True, stdout=None, stderr=subprocess.STDOUT):
+            os.system("rmmod nvme")
+            os.system("insmod dnvme.ko")
+
+        skip_suits = {19, 22, 23, 24}
+        for suiteNum in range(1, 28):
+            if suiteNum not in skip_suits:
+                cmd = "./tnvme --rev=1.2 --test=%d > ./Logs/test%d.txt 2>&1" % (suiteNum, suiteNum)
+                print cmd
+                print "START", int(time.time())
+                res = os.system(cmd)
+                print "STOP", int(time.time())
+                assert 0 == res, "Failed running suit %s" % suiteNum
