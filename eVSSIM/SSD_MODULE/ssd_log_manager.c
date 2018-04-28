6f0527f8f6c7e7802b19ebd426a4adcb38d9046e
David <davidlichterov@gmail.com>
2018-04-28 22:27:05 +0300

    Implements: basic offline logging analyzer
    - added offline logging analyzer
    - implemented the offline logging analyzer function
    - added basic test for logging offline analyzer
    - added logger reduce and clean functions and test for the functions
    - added logger_read_log function that reads single given log
    - updated makefile to compile the new added files
    - updated gitignore to ignore new added files
    
    Change-Id: I5e4583174af91d49f3574740cc04a096f72bd84c

--- eVSSIM/SSD_MODULE/ssd_log_manager.c
+++ eVSSIM/SSD_MODULE/ssd_log_manager.c
@@ -1,4 +1,4 @@
-// Copyright(c)2013 
+// Copyright(c)2013
 //
 // Hanyang University, Seoul, Korea
 // Embedded Software Systems Lab. All right reserved
@@ -24,7 +24,7 @@
 #endif
 
 /**
- * The data allocated for each logger and real time analyzer pair
+ * The data allocated for each logger and real time analyzer/offline analyzer
  */
 typedef struct {
     /**
@@ -36,11 +36,18 @@ typedef struct {
      */
     RTLogAnalyzer* rt_log_analyzer;
     /**
+     * The offline analyzer of the logger pool
+     */
+    OfflineLogAnalyzer* offline_log_analyzer;
+    /**
      * The thread of the real time analyzer
      */
     pthread_t rt_log_analyzer_thread;
+    /**
+     * The thread of the offline log analyzer
+     */
+    pthread_t offline_log_analyzer_thread;
 } LoggerAnalyzerStorage;
-
 
 // old monitor data
 int servSock;
@@ -48,12 +55,7 @@ int clientSock = 0;
 int g_init_log_server = 0;
 FILE *monitor = NULL;
 
-
 // new logging server data
-/**
- * The size of each logger, in bytes (currently ~16Mb)
- */
-#define LOGGER_SIZE (1 << 24)
 /**
  * The loggers' + real time analyzers' data
  */
@@ -71,13 +73,11 @@ pthread_t log_manager_thread;
  */
 pthread_t log_server_thread;
 
-
 void reset_analyzers(void) {
     int i;
     for (i = 0; i < FLASH_NB; i++)
         analyzers_storage[i].rt_log_analyzer->reset_flag = 1;
 }
-
 
 void INIT_LOG_MANAGER(void)
 {
@@ -88,102 +88,114 @@ void INIT_LOG_MANAGER(void)
 			PERR("popen failed: %s\n", strerror(errno));
 		THREAD_SERVER();
 
-		g_init_log_server = 1;
-	}
+        g_init_log_server = 1;
+    }
 #endif
 
-	// handle new logging server
+    // handle new logging server
 #ifdef LOGGING_SERVER_ON
-	int i;
+    int i;
 
-	// allocate memory
-	analyzers_storage = (LoggerAnalyzerStorage*) malloc(sizeof(LoggerAnalyzerStorage) * FLASH_NB);
-	if (analyzers_storage == NULL)
-	    PERR("Couldn't allocate memory for the loggers and real time analyzers: %s\n",
-	         strerror(errno));
+    // allocate memory
+    analyzers_storage = (LoggerAnalyzerStorage*) malloc(sizeof(LoggerAnalyzerStorage) * FLASH_NB);
+    if (analyzers_storage == NULL)
+        PERR("Couldn't allocate memory for the loggers and real time analyzers: %s\n",
+                strerror(errno));
 
-	// init structures
-	for (i = 0; i < FLASH_NB; i++) {
-        // TODO - what is the logger size that we want here ?
-        // how many log's we want to allocate
-	    analyzers_storage[i].logger = logger_init(DEFUALT_LOGGER_POOL_SIZE);
-	    if (analyzers_storage[i].logger == NULL)
-	        PERR("Couldn't create the logger: %s\n", strerror(errno));
-	    analyzers_storage[i].rt_log_analyzer = rt_log_analyzer_init(analyzers_storage[i].logger);
-	    if (analyzers_storage[i].rt_log_analyzer == NULL)
-	        PERR("Couldn't create the real time analyzer: %s\n", strerror(errno));
-	}
-	log_manager = log_manager_init();
-	if (log_manager == NULL)
-	    PERR("Couldn't create the log manager: %s\n", strerror(errno));
-	log_server_init();
-
-	// connect the logging mechanism
-	for (i = 0; i < FLASH_NB; i++) {
-	    log_manager_add_analyzer(log_manager, analyzers_storage[i].rt_log_analyzer);
+    // init structures
+    for (i = 0; i < FLASH_NB; i++) {
+        analyzers_storage[i].logger = logger_init(DEFUALT_LOGGER_POOL_SIZE);
+        if (analyzers_storage[i].logger == NULL)
+            PERR("Couldn't create the logger: %s\n", strerror(errno));
+        analyzers_storage[i].rt_log_analyzer = rt_log_analyzer_init(analyzers_storage[i].logger);
+        if (analyzers_storage[i].rt_log_analyzer == NULL)
+            PERR("Couldn't create the real time analyzer: %s\n", strerror(errno));
+        analyzers_storage[i].offline_log_analyzer = offline_log_analyzer_init(analyzers_storage[i].logger);
+        if (analyzers_storage[i].offline_log_analyzer == NULL)
+            PERR("Couldn't create the offline log analyzer: %s\n", strerror(errno));
     }
-	log_manager_subscribe(log_manager, log_server_update, NULL);
-	log_server_on_reset(reset_analyzers);
 
-	// run the threads
-	pthread_create(&log_server_thread, NULL, log_server_run, NULL);
-	pthread_create(&log_manager_thread, NULL, log_manager_run, log_manager);
-	for (i = 0; i < FLASH_NB; i++) {
-	    pthread_create(&(analyzers_storage[i].rt_log_analyzer_thread), NULL, rt_log_analyzer_run,
-	                   analyzers_storage[i].rt_log_analyzer);
-	}
+    log_manager = log_manager_init();
+    if (log_manager == NULL)
+        PERR("Couldn't create the log manager: %s\n", strerror(errno));
+    log_server_init();
+
+    // connect the logging mechanism
+    for (i = 0; i < FLASH_NB; i++) {
+        log_manager_add_analyzer(log_manager, analyzers_storage[i].rt_log_analyzer);
+    }
+    log_manager_subscribe(log_manager, log_server_update, NULL);
+    log_server_on_reset(reset_analyzers);
+
+    // run the threads
+    pthread_create(&log_server_thread, NULL, log_server_run, NULL);
+    pthread_create(&log_manager_thread, NULL, log_manager_run, log_manager);
+    for (i = 0; i < FLASH_NB; i++) {
+        // run rt log analyzer
+        pthread_create(&(analyzers_storage[i].rt_log_analyzer_thread), NULL, rt_log_analyzer_run,
+                analyzers_storage[i].rt_log_analyzer);
+        // run offline log analyzer
+        pthread_create(&(analyzers_storage[i].offline_log_analyzer_thread), NULL,
+                offline_log_analyzer_run, analyzers_storage[i].offline_log_analyzer);
+    }
 
     PINFO("Log server opened\n");
     PINFO("Browse to http://127.0.0.1:%d/ to see the current statistics\n", LOG_SERVER_PORT);
 #endif
 }
+
 void TERM_LOG_MANAGER(void)
 {
     // handle old monitor
 #ifdef MONITOR_ON
-	close(servSock);
-	close(clientSock);
-	pclose(monitor);
+    close(servSock);
+    close(clientSock);
+    pclose(monitor);
 #endif
 
-	// handle new logging server
+    // handle new logging server
 #ifdef LOGGING_SERVER_ON
-	int i;
+    int i;
 
-	// alert the different threads to stop
-	for (i = 0; i < FLASH_NB; i++)
-	    analyzers_storage[i].rt_log_analyzer->exit_loop_flag = 1;
-	log_manager->exit_loop_flag = 1;
-	log_server.exit_loop_flag = 1;
+    // alert the different threads to stop
+    for (i = 0; i < FLASH_NB; i++)
+    {
+        analyzers_storage[i].rt_log_analyzer->exit_loop_flag = 1;
+        analyzers_storage[i].offline_log_analyzer->exit_loop_flag = 1;
+    }
+    log_manager->exit_loop_flag = 1;
+    log_server.exit_loop_flag = 1;
 
-	// wait for the different threads to stop, and clear their data
-	for (i = 0; i < FLASH_NB; i++) {
-	    pthread_join(analyzers_storage[i].rt_log_analyzer_thread, NULL);
-	    rt_log_analyzer_free(analyzers_storage[i].rt_log_analyzer, 1);
-	}
-	pthread_join(log_manager_thread, NULL);
-	log_manager_free(log_manager);
-	pthread_join(log_server_thread, NULL);
-	log_server_free();
+    // wait for the different threads to stop, and clear their data
+    for (i = 0; i < FLASH_NB; i++) {
+        pthread_join(analyzers_storage[i].rt_log_analyzer_thread, NULL);
+        pthread_join(analyzers_storage[i].offline_log_analyzer_thread, NULL);
+        rt_log_analyzer_free(analyzers_storage[i].rt_log_analyzer, 1);
+        offline_log_analyzer_free(analyzers_storage[i].offline_log_analyzer);
+    }
+    pthread_join(log_manager_thread, NULL);
+    log_manager_free(log_manager);
+    pthread_join(log_server_thread, NULL);
+    log_server_free();
 
-	// free allocated memory
-	free(analyzers_storage);
+    // free allocated memory
+    free(analyzers_storage);
 #endif
 }
 
 void WRITE_LOG(const char *fmt, ...)
 {
 #ifdef MONITOR_ON
-	if (clientSock == 0)
-		RERR(, "write log is failed\n");
+    if (clientSock == 0)
+        RERR(, "write log is failed\n");
 
-	char szLog[1024];
-	va_list argp;
-	va_start(argp, fmt);
-	vsprintf(szLog, fmt, argp);
-	send(clientSock, szLog, strlen(szLog), 0);
-	send(clientSock, "\n", 1, MSG_DONTWAIT);
-	va_end(argp);
+    char szLog[1024];
+    va_list argp;
+    va_start(argp, fmt);
+    vsprintf(szLog, fmt, argp);
+    send(clientSock, szLog, strlen(szLog), 0);
+    send(clientSock, "\n", 1, MSG_DONTWAIT);
+    va_end(argp);
 #endif
 }
 
@@ -194,9 +206,9 @@ void THREAD_SERVER(void)
 
     if ((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         RDBG_MNT(, "socket failed: %s\n", strerror(errno));
-	int flags = fcntl(servSock, F_GETFL, 0);
+    int flags = fcntl(servSock, F_GETFL, 0);
 
-	fcntl(servSock, F_SETFL, flags | O_APPEND);
+    fcntl(servSock, F_SETFL, flags | O_APPEND);
     int option = 1;
     if (setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)) < 0)
         RDBG_MNT(, "setsockopt SO_REUSEADDR failed: %s\n", strerror(errno));
@@ -206,7 +218,7 @@ void THREAD_SERVER(void)
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddr.sin_port = htons(9999);
-  
+
     if (bind(servSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
         RDBG_MNT(, "bind failed: %s\n", strerror(errno));
 
