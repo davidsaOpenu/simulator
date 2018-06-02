2fe1e4a5f77efcbe8ed76e883aa2fe8f64d8a478
David Sariel <davidsa@openu.ac.il>
2018-06-02 23:28:32 +0300

    WIP - main.dsl
    Change-Id: I2cfd63138907dc571445b40b4103645f3cfe5483

---
+++ infra/jjb/Clone.groovy
@@ -0,0 +1,9 @@
+// This is the 'include file'
+// clone.dsl will load it as an implicit class
+// Each method in here will become a method on the implicit class
+
+Class Clone{
+    def myUtilityMethod(String msg) {
+        println "myUtilityMethod running with: ${msg}"
+    }
+}
