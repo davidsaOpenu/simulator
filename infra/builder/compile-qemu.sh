ba9983150b06efff9e1c0cede82d80ae0d2cc052
Shimi Gersner <gersner@gmail.com>
2018-06-16 12:07:52 +0000

    Implements: Support master QEMU with new build environment
    Support for latest QEMU code from https://github.com/davidsaOpenu/qemu.
    
    Change-Id: I0c12c513a48f490e03a090c75b0d21cb4b6c8c93
    Signed-off-by: Shimi Gersner <gersner@gmail.com>

--- infra/builder/compile-qemu.sh
+++ infra/builder/compile-qemu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source ./builder.sh
 
-# Compile osd
-evssim_run_at_folder $EVSSIM_SIMULATOR_FOLDER/eVSSIM/osc-osd make target
-
 # Configure qemu
 evssim_run_at_folder $EVSSIM_QEMU_FOLDER ./configure \
-    --enable-io-thread --enable-linux-aio --target-list=x86_64-softmmu --enable-sdl --enable-vssim \
-    "--extra-cflags='-Wno-error=unused-but-set-variable -Wno-error=deprecated-declarations $COMPILATION_CFLAGS'" \
-    --cc=$COMPILATION_GCC --enable-xen --disable-docs
+    --enable-trace-backends=log \
+    --disable-docs --enable-tools \
+    --enable-linux-aio \
+    --disable-sdl --disable-gtk \
+    --enable-kvm --target-list=x86_64-softmmu \
+    "--extra-cflags='-Wno-error=unused-but-set-variable -Wno-error=deprecated-declarations $COMPILATION_CFLAGS'"
 
 # Make
 evssim_run_at_folder $EVSSIM_QEMU_FOLDER bear make
