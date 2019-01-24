9a4dd80e27a2ba887f3dbb7a7d548d068abce951
Daniel <assa.daniel@gmail.com>
2019-01-24 11:05:22 +0200

    Fix ssd utilization calculation
    - ssd utilization statistic were not take in
      account number of written pages at particular
      block
    - New monitor statistics could not testing using
      of multiple flash
    
    Change-Id: I47270602fd1bd362ce882387cf37d3cab8f320d7

--- eVSSIM/tests/host/rt_analyzer_subscriber.cc
+++ eVSSIM/tests/host/rt_analyzer_subscriber.cc
@@ -140,7 +140,7 @@ namespace rt_subscriber {
                     .read_speed = (1.0 / (REG_READ_DELAY + CELL_READ_DELAY)) * SEC_IN_USEC,
                     .garbage_collection_count = 1,
                     .write_amplification = 1.0,
-                    .utilization = 0.0 / PAGES_IN_SSD
+                    .utilization = 1.0 / PAGES_IN_SSD
             },
             // physical cell read
             {
@@ -150,7 +150,7 @@ namespace rt_subscriber {
                     .read_speed = (2.0 / (REG_READ_DELAY + CELL_READ_DELAY + BLOCK_ERASE_DELAY + CHANNEL_SWITCH_DELAY_R + CELL_READ_DELAY)) * SEC_IN_USEC,
                     .garbage_collection_count = 1,
                     .write_amplification = 1.0,
-                    .utilization = 0.0 / PAGES_IN_SSD
+                    .utilization = 1.0 / PAGES_IN_SSD
             },
             // garbage collection
             {
@@ -160,7 +160,7 @@ namespace rt_subscriber {
                     .read_speed = (2.0 / (REG_READ_DELAY + CELL_READ_DELAY + BLOCK_ERASE_DELAY + CHANNEL_SWITCH_DELAY_R + CELL_READ_DELAY)) * SEC_IN_USEC,
                     .garbage_collection_count = 2,
                     .write_amplification = 1.0,
-                    .utilization = 0.0 / PAGES_IN_SSD
+                    .utilization = 1.0 / PAGES_IN_SSD
             }
     };
 
@@ -213,7 +213,7 @@ namespace rt_subscriber {
         });
         TIME_MICROSEC(end9);
         LOG_BLOCK_ERASE(logger, (BlockEraseLog) {
-            .channel = 25, .die = 26, .block = 27,
+            .channel = 25, .die=26, .block = 27, .erased_pages = 1,
             .metadata = {start, end9}
         });
         TIME_MICROSEC(end10);
