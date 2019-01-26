d1f731cebdb808b205838bfdb18b5f0c108f8044
Daniel <assa.daniel@gmail.com>
2019-01-26 13:18:38 +0200

    Add dynamic GC algorithm implementations
    - Garbage collection algorithm defined statically: add setters for
    'GC collection' & 'get new page' implementions added.
    
    Change-Id: I765c5c1f49120126418e7323c1336155d68fd2d0

--- eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_gc_manager.h
+++ eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_gc_manager.h
@@ -10,7 +10,25 @@
 
 extern unsigned int gc_count;
 
+/**
+ * Init gc manger
+ */
+void INIT_GC_MANAGER(void);
+/**
+ * Set gc collection algorithm
+ */
+void GC_SET_COLLETION_ALGO(ftl_ret_val (*gc_collection)(int, int, bool));
+/**
+ * Set gc get new page algorithm
+ */
+void GC_SET_GET_NEW_PAGE_ALGO(ftl_ret_val (*get_new_page)(int, int, uint32_t*));
+
 void GC_CHECK(unsigned int phy_flash_nb, unsigned int phy_block_nb, bool force, bool isObjectStrategy);
+
+/**
+ * Default garbage collection algorithm
+ */
+ftl_ret_val GARBAGE_COLLECTION_DEFAULT(int mapping_index, int l2, bool isObjectStrategy);
 
 ftl_ret_val GARBAGE_COLLECTION(int mapping_index, int l2, bool isObjectStrategy);
 ftl_ret_val SELECT_VICTIM_BLOCK(unsigned int* phy_flash_nb, unsigned int* phy_block_nb);
@@ -22,5 +40,19 @@ typedef struct write_amplification_counters
 }write_amplification_counters;
 
 extern write_amplification_counters wa_counters;
+
+/**
+ * GC collection algorithm struct
+ */
+typedef struct {
+    /**
+     * Pointer to method on GC_COLLECTION execution
+     */
+    ftl_ret_val (*gc_collection)(int, int, bool);
+    /**
+     * Pointer to method on GET_NEW_PAGE execution
+     */
+    ftl_ret_val (*get_new_page)(int, int, uint32_t*);
+} GCAlgorithm;
 
 #endif
