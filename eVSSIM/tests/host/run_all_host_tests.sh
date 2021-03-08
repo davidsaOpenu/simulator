45aac71e31deba332cdbf6f606dc20291d743d02
FinalHopee <finalhopee@gmail.com>
2021-03-08 09:56:13 -0800

    Simulator Host Tests To Single Executable
    Move host tests in simulator to a single executable
    To run a host test will use a gtest filter
    Default Compilation flags change to reduce test runtime
    Create BaseEmulatorParams class
    Use inheritance for simulator host tests
    
    Change-Id: Ie363f759a7d03ea2d6ab8832c347cbe7b4ab2b35

--- eVSSIM/tests/host/run_all_host_tests.sh
+++ eVSSIM/tests/host/run_all_host_tests.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-
-RESULT=0
+valgrind --leak-check=full --error-exitcode=2 ./host_tests_main --ci
+RESULT=$?
 
 # run sector tests
-valgrind --leak-check=full --error-exitcode=2 ./sector_tests --ci
-CURR_RESULT=$?
+# valgrind --leak-check=full --error-exitcode=2 ./host_tests_main --sector-tests --ci
+# CURR_RESULT=$?
+# if [ $CURR_RESULT -ne 0 ]; then
+#   RESULT=1
+# fi
 
-# run object tests
-# TODO: uncomment when running time issue is fixed
-valgrind --leak-check=full --error-exitcode=2 ./object_tests
-CURR_RESULT=$?
-if [ $CURR_RESULT -ne 0 ]; then
-  RESULT=1
-fi
+# # run object tests
+# valgrind --leak-check=full --error-exitcode=2 ./host_tests_main --object-tests
+# CURR_RESULT=$?
+# if [ $CURR_RESULT -ne 0 ]; then
+#   RESULT=1
+# fi
 
-# run log manager tests
-valgrind --leak-check=full --suppressions=./log_mgr_tests.supp --error-exitcode=2 ./log_mgr_tests
-CURR_RESULT=$?
-if [ $CURR_RESULT -ne 0 ]; then
-  RESULT=1
-fi
+# # run log manager tests
+# valgrind --leak-check=full --error-exitcode=2 ./host_tests_main --log-mgr-tests
+# CURR_RESULT=$?
+# if [ $CURR_RESULT -ne 0 ]; then
+#   RESULT=1
+# fi
 
 
-# run io emulator monitor statistics tests
-# performance tests that incur real delays must run naively
-./ssd_io_emulator_tests
-CURR_RESULT=$?
-if [ $CURR_RESULT -ne 0 ]; then
-  RESULT=1
-fi
+# # run io emulator monitor statistics tests
+# # performance tests that incur real delays must run naively
+# ./host_tests_main --ssd-io-emulator-tests
+# CURR_RESULT=$?
+# if [ $CURR_RESULT -ne 0 ]; then
+#   RESULT=1
+# fi
 
 exit $RESULT
-
-
