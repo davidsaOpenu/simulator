727b6c1a2683457d6a6a81aa82f32aeab98c4170
FinalHopee <finalhopee@gmail.com>
2021-03-08 09:56:13 -0800

    Simulator Host Tests To Single Executable
    Move host tests in simulator to a single executable
    To run a host test will use a gtest filter
    Default Compilation flags change to reduce test runtime
    Create BaseEmulatorParams class
    Use inheritance for simulator host tests
    
    Change-Id: Ie363f759a7d03ea2d6ab8832c347cbe7b4ab2b35

--- infra/builder/docker-run-sanity.sh
+++ infra/builder/docker-run-sanity.sh
@@ -7,10 +7,29 @@ evssim_qemu_fresh_image
 # Run qemu
 evssim_qemu_detached
 
-# Run a command inside
-if evssim_guest ls -al /dev/nvme0n1 2>/dev/null >/dev/null; then
-    echo "eVSSIM Up & Running!"
-else
-    echo "eVSSIM Failed to find /dev/nvme0n1."
-    exit 1
+
+function check_evssim_guest_for_dev_nvme {
+    is_dev_exists=0
+
+    if evssim_guest ls -al /dev/nvme0n1 2>/dev/null >/dev/null; then
+        echo "eVSSIM Up & Running!"
+        is_dev_exists=1
+    else
+        echo "eVSSIM Failed to find /dev/nvme0n1."
+    fi
+    return $is_dev_exists
+}
+
+check_evssim_guest_for_dev_nvme
+status=$?
+
+if [[ $status == 0 ]]; then
+    # Before checking if device nvme0n1 exists we should wait
+    # Otherwise there are sporadic failures on "Failed to find device" error
+    sleep 10
+    echo "Check eVSSIM again.."
+    check_evssim_guest_for_dev_nvme
+    if [[ $? == 0 ]]; then
+        exit 1
+    fi
 fi
