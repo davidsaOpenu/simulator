bf07daab73a8535cf9e9c71657b2baaabdb833b2
FinalHopee <finalhopee@gmail.com>
2021-03-08 09:56:13 -0800

    Simulator Host Tests To Single Executable
    Move host tests in simulator to a single executable
    To run a host test will use a gtest filter
    Default Compilation flags change to reduce test runtime
    Create BaseEmulatorParams class
    Use inheritance for simulator host tests
    
    Change-Id: Ie363f759a7d03ea2d6ab8832c347cbe7b4ab2b35

--- eVSSIM/tests/host/sector_tests.cc
+++ eVSSIM/tests/host/sector_tests.cc
@@ -1,79 +1,77 @@
 
 #include "base_emulator_tests.h"
 
-int main(int argc, char **argv) {
-    // check if CI_MODE environment variable is set to NIGHTLY
-    const char* ci_mode = getenv("CI_MODE");
-    if(ci_mode != NULL && std::string(ci_mode) == "NIGHTLY")
-        g_nightly_mode = true;
-
-    for (int i = 0; i < argc; i++) {
-        if (strcmp(argv[i], "--nightly") == 0) {
-            g_nightly_mode = true;
-        }
-    }
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
+extern bool g_nightly_mode;
 
 using namespace std;
 
 namespace {
 
-    class SectorUnitTest : public BaseEmulatorTests {}; // OccupySpaceStressTest
+    class SectorUnitTest : public BaseTest {
+        public:
+            virtual void SetUp() {
+                SSDConf test_params = GetParam();
+                BaseTest::SetUp(test_params);
+                INIT_LOG_MANAGER();
+            }
 
-    std::vector<std::pair<size_t,size_t> > GetParams() {
-        std::vector<std::pair<size_t,size_t> > list;
+            virtual void TearDown() {
+                BaseTest::TearDown();
+                TERM_LOG_MANAGER();
+            }
+    }; // OccupySpaceStressTest
+
+    std::vector<SSDConf> GetTestParams() {
+        std::vector<SSDConf> test_params;
 
         if (g_nightly_mode) {
             printf("Running in Nightly mode\n");
 
-            for( unsigned int i = 0;
-                    i < sizeof(parameters::Allsizemb)/sizeof(parameters::Allsizemb[0]); i++ )
-            {
-                for( unsigned int j = 0;
-                        j < sizeof(parameters::Allflashnb)/sizeof(parameters::Allflashnb[0]); j++ )
-                    // first paramter in the pair is size of disk in mb
-                    // second paramter in the pair is number of flash memories in ssd
-                    list.push_back(std::make_pair(parameters::Allsizemb[i], parameters::Allflashnb[j] ));
+            for (unsigned int i = 0; i < sizeof(parameters::Allsizemb)/sizeof(parameters::Allsizemb[0]); i++) {
+                for (unsigned int j = 0; j < sizeof(parameters::Allflashnb)/sizeof(parameters::Allflashnb[0]); j++) {
+                    test_params.push_back(SSDConf(parameters::Allsizemb[i], parameters::Allflashnb[j]));
+                }
             }
-
-            return list;
+        } else {
+            for (unsigned int i = 0; i < sizeof(parameters::Allsizemb)/sizeof(parameters::Allsizemb[0]); i++) {
+                test_params.push_back(SSDConf(parameters::Allsizemb[i], DEFAULT_FLASH_NB));
+            }
         }
 
-        const int constFlashNum = DEFAULT_FLASH_NB;
-        for( unsigned int i = 0;
-                i < sizeof(parameters::Allsizemb)/sizeof(parameters::Allsizemb[0]); i++ )
-            list.push_back(std::make_pair(parameters::Allsizemb[i], constFlashNum ));
-
-        return list;
+        return test_params;
     }
 
-    INSTANTIATE_TEST_CASE_P(DiskSize, SectorUnitTest, ::testing::ValuesIn(GetParams()));
+    INSTANTIATE_TEST_CASE_P(DiskSize, SectorUnitTest, ::testing::ValuesIn(GetTestParams()));
+
     TEST_P(SectorUnitTest, SequentialOnePageAtTimeWrite) {
+        SSDConf test_params = GetParam();
+
         for(int x=0; x<2 /*8*/; x++){
-            for(size_t p=0; p < pages_; p++){
-                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT(p * CONST_PAGE_SIZE_IN_BYTES, 1));
+            for(size_t p=0; p < test_params.get_pages(); p++){
+                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT(p * test_params.get_page_size(), 1));
             }
         }
     }
 
     TEST_P(SectorUnitTest, RandomOnePageAtTimeWrite) {
+        SSDConf test_params = GetParam();
+
         for(int x=0; x<2 /*8*/; x++){
-            for(size_t p=0; p < pages_; p++){
-                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT((rand() % pages_) * CONST_PAGE_SIZE_IN_BYTES, 1));
+            for(size_t p=0; p < test_params.get_pages(); p++){
+                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT((rand() % test_params.get_pages()) * test_params.get_page_size(), 1));
             }
         }
     }
 
     TEST_P(SectorUnitTest, MixSequentialAndRandomOnePageAtTimeWrite) {
+        SSDConf test_params = GetParam();
+
         for(int x=0; x<2; x++){
-            for(size_t p=0; p < pages_; p++){
-                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT((rand() % pages_) * CONST_PAGE_SIZE_IN_BYTES, 1));
+            for(size_t p=0; p < test_params.get_pages(); p++){
+                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT((rand() % test_params.get_pages()) * test_params.get_page_size(), 1));
             }
-            for(size_t p=0; p < pages_; p++){
-                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT(p * CONST_PAGE_SIZE_IN_BYTES, 1));
+            for(size_t p=0; p < test_params.get_pages(); p++){
+                ASSERT_EQ(FTL_SUCCESS, _FTL_WRITE_SECT(p * test_params.get_page_size(), 1));
             }
         }
     }
