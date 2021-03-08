b12a45d1493fd182e47c774e24724f1c3027d321
FinalHopee <finalhopee@gmail.com>
2021-03-08 09:56:13 -0800

    Simulator Host Tests To Single Executable
    Move host tests in simulator to a single executable
    To run a host test will use a gtest filter
    Default Compilation flags change to reduce test runtime
    Create BaseEmulatorParams class
    Use inheritance for simulator host tests
    
    Change-Id: Ie363f759a7d03ea2d6ab8832c347cbe7b4ab2b35

--- infra/builder/docker-run-sanity.sh
+++ infra/builder/docker-run-sanity.sh
@@ -7,7 +7,11 @@ evssim_qemu_fresh_image
 # Run qemu
 evssim_qemu_detached
 
-# Run a command inside
+# Before checking if device nvme0n1 exists we should wait
+# Otherwise there are sporadic failures on "Failed to find device" error
+sleep 10
+    
+# Run a command inside the container (check if device nvme0n1 exists)
 if evssim_guest ls -al /dev/nvme0n1 2>/dev/null >/dev/null; then
     echo "eVSSIM Up & Running!"
 else
