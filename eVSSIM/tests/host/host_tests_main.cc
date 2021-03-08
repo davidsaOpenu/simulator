5946ffb6958461cacc4c31a3481f9d3d7b727622
FinalHopee <finalhopee@gmail.com>
2021-03-08 09:56:13 -0800

    Simulator Host Tests To Single Executable
    Move host tests in simulator to a single executable
    To run a host test will use a gtest filter
    Default Compilation flags change to reduce test runtime
    Create BaseEmulatorParams class
    Use inheritance for simulator host tests
    
    Change-Id: Ie363f759a7d03ea2d6ab8832c347cbe7b4ab2b35

---
+++ eVSSIM/tests/host/host_tests_main.cc
@@ -0,0 +1,33 @@
+#include "base_emulator_tests.h"
+
+using namespace std;
+
+bool g_ci_mode = false;
+bool g_monitor_mode = false;
+bool g_server_mode = false;
+
+int main(int argc, char **argv) {
+    string tests_filter = "*";
+
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "--ci") == 0) {
+            g_ci_mode = true;
+        } else if (strcmp(argv[i], "--show-monitor") == 0) {
+            g_monitor_mode = true;
+        } else if (strcmp(argv[i], "--run-server") == 0) {
+            g_server_mode = true;
+        } else if (strcmp(argv[i], "--sector-tests") == 0) {
+            tests_filter = "*SectorUnitTest*";
+        } else if (strcmp(argv[i], "--object-tests") == 0) {
+            tests_filter = "*ObjectUnitTest*";
+        } else if (strcmp(argv[i], "--log-mgr-tests") == 0) {
+            tests_filter = "*LogMgrUnitTest*";
+        } else if (strcmp(argv[i], "--ssd-io-emulator-tests") == 0) {
+            tests_filter = "*SSDIoEmulatorUnitTest*";
+        }
+    }
+
+    testing::GTEST_FLAG(filter) = tests_filter;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
