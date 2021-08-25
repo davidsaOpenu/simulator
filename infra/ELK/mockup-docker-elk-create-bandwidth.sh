f9154fe65f2f86975721141dd3218cfc5c108dd7
Ilan Kozlov <ilankozlov@gmail.com>
2021-08-25 11:00:35 +0300

    Adding mockup script for dynamic log generation, currently just cp the log file to logs uploading directory
    Change-Id: Ie30a4965e2c2c4fc4459750c23164a3693a5cead

--- infra/ELK/mockup-docker-elk-create-bandwidth.sh
+++ infra/ELK/mockup-docker-elk-create-bandwidth.sh
@@ -1,6 +1,6 @@
 cp real_test_log.json logs/.
 
   
-export $start_time="2021-07-04T15:14:47.000Z"
-export $end_time="2021-07-04T15:15:00.000Z"
-export $log_file="real_test_log.json"
+export start_time="2021-07-04T15:14:47.000Z"
+export end_time="2021-07-04T15:15:00.000Z"
+export log_file="real_test_log.json"
