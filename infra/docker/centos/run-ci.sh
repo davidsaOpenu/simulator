08de953efdabbc687f7ce7e1cf5fad995490a8f4
David Sariel <davidsa@openu.ac.il>
2022-08-26 12:14:55 +0300

    Add run-ci.sh script
    that will be invoked by the following jobs:
    - simulator-full-docker-ubuntu-new
    - simulator-full-docker-centos-new
    
    Change-Id: Id6161004974f1fc04ec86c8bbc6f7bd6e1455afd

---
+++ infra/docker/centos/run-ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ${WORKSPACE}/simulator
+
+echo RUN groupadd `stat -c "%G" .` -g `stat -c "%g" .` >> ${WORKSPACE}/simulator/infra/docker/centos/Dockerfile
+echo RUN useradd -ms /bin/bash `stat -c "%U" .` -u `stat -c "%u" .` -g `stat -c "%g" .` -G wheel >> ${WORKSPACE}/simulator/infra/docker/centos/Dockerfile
+
+docker build -t os-centos ${WORKSPACE}/simulator/infra/docker/centos
+
+
+[ ! -d ${WORKSPACE}/hda/ ] && mkdir ${WORKSPACE}/hda/
+ls -all ${WORKSPACE}/hda/
+if [[ ! -f ${WORKSPACE}/hda/hda_clean.qcow2 ]]
+then
+  cp ${WORKSPACE}/../hda/hda_clean.qcow2  ${WORKSPACE}/hda
+fi
+
+docker run --rm -u `stat -c "%u:%g" .` -i  -v /tmp/.X11-unix:/tmp/.X11-unix -v ${WORKSPACE}:/code --cap-add SYS_PTRACE --privileged=true os-centos /bin/bash -C "/code/simulator/infra/docker/centos/run-ansible"
+
+# run cppcheck (ignore for now)
+#/opt/cppcheck/cppcheck --enable=all --inconclusive --xml --xml-version=2  ${WORKSPACE}/simulator/eVSSIM/QEMU/hw 2> ${WORKSPACE}/cppcheck.xml
