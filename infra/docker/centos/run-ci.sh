8e41e93c59d44c2cfd77344e4fcbce0fcf5fe398
Yannay Hammer <yannayha@gmail.com>
2022-10-01 19:16:27 +0300

    Running elk stack on newer build system CI.
    Also cleans up the elk env using TRAP
    
    Change-Id: I9471d49343440e599f31629958adce0594681dd0

--- infra/docker/centos/run-ci.sh
+++ infra/docker/centos/run-ci.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+function cleanup_elk() {
+  pushd ${WORKSPACE}/simulator/infra/docker/elk
+  $DOCKER_COMPOSE_BINARY -p $DOCKER_COMPOSE_PROJECT down
+  popd
+}
+
+
 export DOCKER_COMPOSE_PROJECT='evssim-elk-centos'
 DOCKER_COMPOSE_BINARY=`whereis -b docker-compose | cut -d ' ' -f2`
 
@@ -12,7 +19,6 @@ echo RUN useradd -ms /bin/bash `stat -c "%U" .` -u `stat -c "%u" .` -g `stat -c 
 
 docker build -t os-centos ${WORKSPACE}/simulator/infra/docker/centos
 
-
 [ ! -d ${WORKSPACE}/hda/ ] && mkdir ${WORKSPACE}/hda/
 ls -all ${WORKSPACE}/hda/
 if [[ ! -f ${WORKSPACE}/hda/hda_clean.qcow2 ]]
@@ -21,11 +27,12 @@ then
 fi
 
 pushd ${WORKSPACE}/simulator/infra/docker/elk
+
+trap cleanup_elk EXIT
 $DOCKER_COMPOSE_BINARY -p $DOCKER_COMPOSE_PROJECT up --detach
 
 docker run --network=$DOCKER_COMPOSE_PROJECT --rm -u `stat -c "%u:%g" .` -i  -v /tmp/.X11-unix:/tmp/.X11-unix -v ${WORKSPACE}:/code --cap-add SYS_PTRACE --privileged=true os-centos /bin/bash -C "/code/simulator/infra/docker/centos/run-ansible"
 
-$DOCKER_COMPOSE_BINARY -p $DOCKER_COMPOSE_PROJECT down
 popd
 
 # run cppcheck (ignore for now)
