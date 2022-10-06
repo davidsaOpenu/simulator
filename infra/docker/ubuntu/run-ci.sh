bbd5aaed7a740becd007336811022c72defc0248
Yannay Hammer <yannayha@gmail.com>
2022-10-06 21:10:40 +0300

    Created basic elk tests, modified project structure
    - Moved old build system run-ci.sh to seperate script to reduce code
      duplication
    - Refactored log_indexing_test.py -> split to functions
    - Made old build system run ELK stack tests as well
    
    Change-Id: Idec262e17c93cc0ab0fd190b8cd94df3173f6e62

--- infra/docker/ubuntu/run-ci.sh
+++ infra/docker/ubuntu/run-ci.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
-set -e
+export CI_OPERATING_SYSTEM=ubuntu
+export SUPERUSER_GROUP=sudo
 
-function cleanup_elk() {
-  pushd ${WORKSPACE}/simulator/infra/docker/elk
-  $DOCKER_COMPOSE_BINARY -p $DOCKER_COMPOSE_PROJECT down
-  popd
-}
-
-
-export DOCKER_COMPOSE_PROJECT='evssim-elk-ubuntu'
-DOCKER_COMPOSE_BINARY=`whereis -b docker-compose | cut -d ' ' -f2`
-
-cd ${WORKSPACE}/simulator
-
-echo RUN groupadd `stat -c "%G" .` -g `stat -c "%g" .` >> ${WORKSPACE}/simulator/infra/docker/ubuntu/Dockerfile
-echo RUN useradd -ms /bin/bash `stat -c "%U" .` -u `stat -c "%u" .` -g `stat -c "%g" .` -G sudo >> ${WORKSPACE}/simulator/infra/docker/ubuntu/Dockerfile
-
-docker build -t os-ubuntu ${WORKSPACE}/simulator/infra/docker/ubuntu
-
-[ ! -d ${WORKSPACE}/hda/ ] && mkdir ${WORKSPACE}/hda/
-ls -all ${WORKSPACE}/hda/
-if [[ ! -f ${WORKSPACE}/hda/hda_clean.qcow2 ]]
-then
-  cp ${WORKSPACE}/../hda/hda_clean.qcow2  ${WORKSPACE}/hda
-fi
-
-pushd ${WORKSPACE}/simulator/infra/docker/elk
-
-trap cleanup_elk EXIT
-$DOCKER_COMPOSE_BINARY -p $DOCKER_COMPOSE_PROJECT up --detach
-
-docker run --network=$DOCKER_COMPOSE_PROJECT --rm -u `stat -c "%u:%g" .` -i  -v /tmp/.X11-unix:/tmp/.X11-unix -v ${WORKSPACE}:/code --cap-add SYS_PTRACE --privileged=true os-ubuntu /bin/bash -C "/code/simulator/infra/docker/ubuntu/run-ansible"
-
-popd
-
-# run cppcheck (ignore for now)
-#/opt/cppcheck/cppcheck --enable=all --inconclusive --xml --xml-version=2  ${WORKSPACE}/simulator/eVSSIM/QEMU/hw 2> ${WORKSPACE}/cppcheck.xml
-
-
+${WORKSPACE}/simulator/infra/docker/run-ci.sh
