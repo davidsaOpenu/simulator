3cdad07d7a3cb74e55509c19a1cb63a0ee8bfa0e
FlameyBoi <nataniel521@gmail.com>
2023-07-21 02:42:21 -0400

    Simple tests for nvme object functions
    -Added a few simple tests for each of the four object related functions:
    objw, objr, objd, objl.
    -Added missing pre-requisistes to host-tests README and fixed old instructions.
    -Removed old tests and related files.
    -Removed exclusion of object test from old infra.
    
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/3.16.2/+/556286
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/nvme-cli/+/553398
    Change-Id: I32dd77d7873d481d6b2eb724e9099282080226d3

--- eVSSIM/tests/guest/run_all_guest_tests.sh
+++ eVSSIM/tests/guest/run_all_guest_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo nosetests -v --with-xunit --xunit-file=guest_tests_results.xml  --exclude==objects_via_ioctl
+sudo nosetests -v --with-xunit --xunit-file=guest_tests_results.xml
 exit $?
