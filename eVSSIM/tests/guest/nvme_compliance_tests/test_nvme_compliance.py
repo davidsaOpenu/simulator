12d683f91b167c4bd9538918ff1edfc0857d7eea
FlameyBoi <nataniel521@gmail.com>
2023-09-10 13:23:19 -0400

    Re-enable nvmecompl suite 24
    -Suite number 24 removed from skip_suits
    -Added option to collect dnvme logs from kernel ring buffer
    
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/qemu/+/1168750
    Change-Id: I0fb4a59e111edfbd2d79c04189b5da4d1e8a4f58

--- eVSSIM/tests/guest/nvme_compliance_tests/test_nvme_compliance.py
+++ eVSSIM/tests/guest/nvme_compliance_tests/test_nvme_compliance.py
@@ -13,6 +13,11 @@ new_qemu_only = unittest.skipIf(not VSSIM_NEXTGEN_BUILD_SYSTEM, reason="New qemu
 NVME_COMPLIANCE_TEST_DIR = "/home/esd/nvmeCompl/"
 TEST_DIR = "/home/esd/guest/nvme_compliance_tests"
 
+# Set to true and define DEBUG in sysdnvme.h to collect dnvme kernel log
+# Note that having debug on considerably slows down testing, specifically suite 4
+DEBUG = False
+
+
 class TestNVMeCompliance:
     @old_qemu_only
     def test_NVMeCompliance_old(self):
@@ -34,6 +39,7 @@ class TestNVMeCompliance:
                 cmd = "./tnvme --test=%d --skiptest=skipTests > test%d.txt 2>&1" % (suiteNum, suiteNum)
                 print cmd
                 assert 0 == os.system(cmd)
+    
 
     @new_qemu_only
     def test_NVMeCompliance_new(self):
@@ -42,12 +48,33 @@ class TestNVMeCompliance:
             os.system("rmmod nvme")
             os.system("insmod dnvme.ko")
 
-        skip_single_tests = ['24:1.0.0']
+        skip_single_tests = []
         with open("skipTests", "w+") as f:
             for test in skip_single_tests:
                 f.write("%s\n" % test)
-
+        
         skip_suits = {}
+        if DEBUG:
+            self.with_kernel_log(skip_suits)
+        else:
+            self.no_kernel_log(skip_suits)
+                
+    def with_kernel_log(self, skip_suits):
+        for suiteNum in range(1, 28):
+            if suiteNum not in skip_suits:
+                cmd = "./tnvme --rev=1.2 --test=%d --skiptest=skipTests > ./Logs/test%d.txt 2>&1" % (suiteNum, suiteNum)
+                dump_kernel = "./Logs/kdump%d.txt" % (suiteNum)
+                print cmd
+                dump_file = open(dump_kernel, 'w')
+                proc = subprocess.Popen(["./log_kernel.sh"], stdout = dump_file)
+                print "START", int(time.time())
+                res = os.system(cmd)
+                print "STOP", int(time.time())
+                time.sleep(2)
+                proc.kill()
+                assert 0 == res, "Failed running suit %s" % suiteNum
+                
+    def no_kernel_log(self, skip_suits):
         for suiteNum in range(1, 28):
             if suiteNum not in skip_suits:
                 cmd = "./tnvme --rev=1.2 --test=%d --skiptest=skipTests > ./Logs/test%d.txt 2>&1" % (suiteNum, suiteNum)
