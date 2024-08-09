b75f99a8aeeb49ea2de946fcea6628237c690b9c
Shai Lahmish <lahmish010@gmail.com>
2024-08-09 16:37:06 +0300

    Support OSD emulation in QEMU
    Integrates open-osd above Linux on QEMU to support formatting
    exoFS images using emulated OSD logical unit (osc-osd project).
    This change adds mkfs.exofs executable into the Linux filesystem
    above the QEMU.
    In addition, this change add run_osd_emulator_and_mount_exofs.sh
    at /bin to allow simple usage. This script starts the OSD emulation,
    initializes the iSCSI interface and mount new formatted exoFS
    on the emulated device /dev/osd0.
    
    Co-authored-by: Amit Shoham <amitshoham8950@gmail.com>
    
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/kernel/+/1199275
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/qemu/+/1199688
    
    Change-Id: Id1204a5893b2e7be69cba2a4253c646c5f1843fb

---
+++ eVSSIM/osc-osd/tgt/usr/bsd/sys/statfs.h
@@ -0,0 +1,7 @@
+#ifndef __TGTBSD_SYS_STATFS_H__
+#define __TGTBSD_SYS_STATFS_H__
+
+/* FreeBSD and Darwin put statfs in <sys/mount.h> */
+#include <sys/mount.h>
+
+#endif /* __TGTBSD_SYS_STATFS_H__ */
