6ab29c663c399ce55e94b447b07c6f2a6fe36e60
Shai Lahmish <lahmish010@gmail.com>
2024-08-09 16:37:06 +0300

    Support OSD emulation in QEMU
    Integrates open-osd above Linux on QEMU to support formatting
    exoFS images using emulated OSD logical unit (osc-osd project).
    This change adds mkfs.exofs executable into the Linux filesystem
    above the QEMU.
    In addition, this change adds run_osd_emulator_and_mount_exofs.sh,
    this script starts the OSD emulation, initializes the iSCSI
    interface and mount new formatted exoFS on the emulated device
    /dev/osd0.
    
    Co-authored-by: Amit Shoham <amitshoham8950@gmail.com>
    
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/kernel/+/1199275
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/qemu/+/1199688
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/open-osd/+/1200046
    
    Change-Id: Id1204a5893b2e7be69cba2a4253c646c5f1843fb

---
+++ eVSSIM/scripts/run_osd_emulator_and_mount_exofs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+osd_dev=/dev/osd0
+mount_point=~/files
+pid=0x10000
+
+echo "> Installing dependencies..."
+sudo apt install -y open-iscsi open-iscsi-utils
+
+
+echo "> Setup OSD emulation..."
+cd ~/osc-osd/
+echo -n "
+NUM_TARGETS=1
+LOG_FILE=./otgtd.log
+BACKSTORE[1]=/var/otgt/otgt-1
+" > ./up.conf;
+sudo ./up
+cd -
+
+sudo iscsiadm -m discovery -t st -p 127.0.0.1
+sudo iscsiadm -m node -T esd-.var.otgt.otgt-1 -p 127.0.0.1 --login
+
+echo "> Creating exofs image at $osd_dev..."
+sudo mkfs.exofs --pid=$pid --dev $osd_dev
+
+echo "> Mounting exofs on $mount_point..."
+sudo mkdir -p $mount_point
+sudo mount -t exofs -o pid=$pid $osd_dev $mount_point
+
+echo "> Done!"
