9b5d2ee9d79828aa279396752a68ce1f6a79617f
Shai Lahmish <lahmish010@gmail.com>
2024-08-27 14:15:02 +0300

    Initial integration of NVMe, replacing OSD
    This change sets up iscsiadm and traces mount, open, close
    operations. Useful for reaserch spike.
    
    In addition it also completes one small step forward for the
    implementation of exofs over NVMe by creating exofs
    on /dev/nvme0n1.
    
    Co-authored-by: Amit Shoham <amitshoham8950@gmail.com>
    Co-authored-by: Rami Krel <rlmk1997@gmail.com>
    Co-authored-by: Adam Zats <adamrnd2@gmail.com>
    
    Trello: https://trello.com/c/Gt8GxnCb/102-format-and-mount-a-new-exofs-image-using-nvme-device-instead-of-osd-device
    
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/kernel/+/1200079
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/open-osd/+/1200073
    
    Change-Id: I904eef4f5208842e742def14704ab4c29e2672f3

--- infra/builder/compile-qemu.sh
+++ infra/builder/compile-qemu.sh
@@ -16,8 +16,8 @@ evssim_run_at_folder $EVSSIM_QEMU_FOLDER make clean
 evssim_run_at_folder $EVSSIM_QEMU_FOLDER bear -- make -j8
 
 # Build osc-osd
-evssim_run_at_folder "$EVSSIM_SIMULATOR_FOLDER/eVSSIM/osc-osd" "make ARCH=x86_64 clean && \
-    make ARCH=x86_64 -j\`nproc\` && \
+evssim_run_at_folder "$EVSSIM_SIMULATOR_FOLDER/eVSSIM/osc-osd" "make MK_PATH=. ARCH=x86_64 clean && \
+    make MK_PATH=. ARCH=x86_64 -j\`nproc\` && \
     rsync -av --progress --exclude='.git' ./ $EVSSIM_DOCKER_ROOT_PATH/$EVSSIM_DIST_FOLDER/osc-osd/"
 
 # Build mkfs.exofs executable and the shared lib libosd (required by mkfs.exofs)
@@ -30,5 +30,5 @@ evssim_run_at_folder "open-osd" "make KSRC=/code/kernel ARCH=x86_64 clean && \
     cp lib/libosd.so $EVSSIM_DOCKER_ROOT_PATH/$EVSSIM_DIST_FOLDER/libosd.so"
 
 # Copy OSD emulation and exofs setup script into the dist directory
-evssim_run_at_folder "$EVSSIM_SIMULATOR_FOLDER/eVSSIM/scripts" "cp run_osd_emulator_and_mount_exofs.sh \
-    $EVSSIM_DOCKER_ROOT_PATH/$EVSSIM_DIST_FOLDER/run_osd_emulator_and_mount_exofs.sh"
+evssim_run_at_folder "$EVSSIM_SIMULATOR_FOLDER/eVSSIM/scripts" "cp -r exofs \
+    $EVSSIM_DOCKER_ROOT_PATH/$EVSSIM_DIST_FOLDER/exofs"
