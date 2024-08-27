f04314042c41efa8c021ae798b5bd001bfc85a9e
Shai Lahmish <lahmish010@gmail.com>
2024-08-27 14:15:02 +0300

    Initial integration of NVMe, replacing OSD
    This change modifies the script that format and mount a new exoFS
    image. The script uses NVMe device instead of OSD device.
    This patch begins the transition of exofs from using OSD devices to NVMe
    devices for mounting and file operations.
    
    Co-authored-by: Amit Shoham <amitshoham8950@gmail.com>
    
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/kernel/+/1200079
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/open-osd/+/1200073
    
    Change-Id: I904eef4f5208842e742def14704ab4c29e2672f3

--- eVSSIM/scripts/run_osd_emulator_and_mount_exofs.sh
+++ eVSSIM/scripts/run_osd_emulator_and_mount_exofs.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 set -e
 
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+  echo "Please execute 'sudo su' before executing this script."
+  exit 1
+fi
+
 osd_dev=/dev/osd0
-mount_point=~/files
+nvme_dev=/dev/nvme0n1
+mount_point=/home/esd/files
 pid=0x10000
+
+cd guest
+./nvme set-feature /dev/nvme0n1 -f 0xc0 --value=1
 
 echo "> Installing dependencies..."
 sudo apt install -y open-iscsi open-iscsi-utils
 
 
 echo "> Setup OSD emulation..."
-cd ~/osc-osd/
+cd /home/esd/osc-osd/
 echo -n "
 NUM_TARGETS=1
 LOG_FILE=./otgtd.log
@@ -23,8 +33,8 @@ cd -
 sudo iscsiadm -m discovery -t st -p 127.0.0.1
 sudo iscsiadm -m node -T esd-.var.otgt.otgt-1 -p 127.0.0.1 --login
 
-echo "> Creating exofs image at $osd_dev..."
-sudo mkfs.exofs --pid=$pid --dev $osd_dev
+echo "> Creating exofs image at $nvme_dev..."
+sudo mkfs.exofs --pid=$pid --dev $nvme_dev
 
 echo "> Mounting exofs on $mount_point..."
 sudo mkdir -p $mount_point
