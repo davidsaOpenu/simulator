a59aaafaea5ce9d364fffcf2a48d4a74117496e0
David Sariel <davidsa@openu.ac.il>
2025-07-11 08:45:01 +0300

    Use multiline comment in run-ci.sh
    Change-Id: Id57294785474ccdd4ae154534220394987fc4594

--- infra/builder/run-ci.sh
+++ infra/builder/run-ci.sh
@@ -7,12 +7,10 @@ source ./elk.sh
 # Make sure every run we get a new clear runtime folder
 export EVSSIM_RUNTIME_ALWAYS_RESET=yes
 
-: <<'END_COMMENT'
 # We source this script since it defines some env-vars that we need, such as ELK ports and container names
 ./build-elk-test-image.sh
 source ./elk-run-stack.sh
 ./elk-run-tests.sh
-END_COMMENT
 
 # Build images
 ./build-docker-image.sh
@@ -23,14 +21,16 @@ END_COMMENT
 ./compile-qemu.sh
 ./compile-host-tests.sh
 ./compile-guest-tests.sh
-
 # Run sanity and tests
 ./docker-run-sanity.sh
 
 ./docker-test-host.sh
 ./docker-test-guest.sh
+
+: <<'END_COMMENT'
 # TODO reenable after the fix 
-# ./docker-test-exofs.sh
+./docker-test-exofs.sh
+END_COMMENT
 
 # ELK stack tests
 trap ./elk-stop-stack.sh EXIT
