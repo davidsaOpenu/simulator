54e639927032f40d0acdd4f6d4e57d03cca1d40a
Liel Harel <liel.harel@gmail.com>
2025-09-20 15:47:25 +0300

    Implement ONFI_READ function and add tests
    Trello Card: https://trello.com/c/3o9jb9Yd/23-onfi-implement-onfiread-function
    
    Change-Id: I1b2eba494690de13a0f7f1b340cd03ee88f772fa

--- eVSSIM/CONFIG/vssim_config_manager.h
+++ eVSSIM/CONFIG/vssim_config_manager.h
@@ -88,6 +88,7 @@ void TERM_SSD_CONFIG(void);
 char* GET_FILE_NAME(uint8_t device_index);
 uint32_t GET_SECTOR_SIZE(uint8_t device_index);
 uint32_t GET_PAGE_SIZE(uint8_t device_index);
+uint64_t GET_PAGE_NB(uint8_t device_index);
 ssd_config_t* GET_DEVICES(void);
 char* GET_DATA_FILENAME(uint8_t device_index, const char* filename);
 
