c6b74fdbfa134a65f2a60cea548367a7af119816
Alon Attia <alon44a@gmail.com>
2025-11-18 22:37:51 +0200

    Add namespace support in eVSSIM
    The config parser now supports namespace definitions.
    For each configured namespace, it creates a new struct based on the namespace type (sector or object).
    This enables support for multiple namespaces with type-specific configurations and mapping tables.
    
    Internal data structures and test logic have been updated to accommodate the new design,
    enabling more flexible and realistic simulation of namespace-aware storage behavior.
    
    Depends-On: https://review.gerrithub.io/c/davidsaOpenu/qemu/+/1223542
    
    See more details in the Trello card:
    https://trello.com/c/nnTUKcEH/67-update-mapping-tables-with-a-namespace-field
    https://trello.com/c/Hr9wNiQH/33-update-namespace-configuration
    https://trello.com/c/Z6ltiZot/40-refactor-the-old-object-strategy-functions
    https://trello.com/c/2vudPDET/34-support-namespace-with-sector-strategy
    
    Change-Id: I11874abacc4a68dc2437ca3c7ab303deb2f7400b

--- eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_mapping_manager.h
+++ eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_mapping_manager.h
@@ -7,18 +7,19 @@
 #define _MAPPING_MANAGER_H_
 
 #include "ftl.h"
+#include "common.h"
 
-extern uint64_t** mapping_table;
+extern uint64_t*** mapping_table;
 
 void INIT_MAPPING_TABLE(uint8_t device_index);
 void TERM_MAPPING_TABLE(uint8_t device_index);
 
-uint64_t GET_MAPPING_INFO(uint8_t device_index, uint64_t lpn);
+uint64_t GET_MAPPING_INFO(uint8_t device_index, uint32_t nsid, uint64_t lpn);
 ftl_ret_val GET_NEW_PAGE(uint8_t device_index, int mode, uint64_t mapping_index, uint64_t* ppn);
 ftl_ret_val DEFAULT_NEXT_PAGE_ALGO(uint8_t device_index, int mode, uint64_t mapping_index, uint64_t* ppn);
 
-int UPDATE_OLD_PAGE_MAPPING(uint8_t device_index, uint64_t lpn);
-int UPDATE_NEW_PAGE_MAPPING(uint8_t device_index, uint64_t lpn, uint64_t ppn);
+int UPDATE_OLD_PAGE_MAPPING(uint8_t device_index, uint32_t nsid, uint64_t lpn);
+int UPDATE_NEW_PAGE_MAPPING(uint8_t device_index, uint32_t nsid, uint64_t lpn, uint64_t ppn);
 int UPDATE_NEW_PAGE_MAPPING_NO_LOGICAL(uint8_t device_index, uint64_t ppn);
 
 unsigned int CALC_FLASH(uint8_t device_index, uint64_t ppn);
@@ -28,4 +29,4 @@ unsigned int CALC_PLANE(uint8_t device_index, uint64_t ppn);
 unsigned int CALC_CHANNEL(uint8_t device_index, uint64_t ppn);
 unsigned int CALC_SCOPE_FIRST_PAGE(uint8_t device_index, uint64_t address, int scope);
 
-#endif
+#endif // _MAPPING_MANAGER_H_
