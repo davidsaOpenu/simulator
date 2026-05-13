864921a3f9948f919eee2c7a451a9e53adb6ad85
Yuri Khordal <yuriKhordal@proton.me>
2026-05-13 20:34:09 +0300

    Add an EcDSA SSH key to guest
    On some machines, the old `id_rsa` key does not work. Adding an Ed25519
    key to fix the issue. The old RSA key is kept as a fallthrough key for
    backwards compatibility, in case there will be some machines that have a
    problem with the new key.
    
    Changes:
    docker/id_ed25519, docker/id_ed25519.pub [NEW]:
    + Added an Ed25519 key, no passphrase.
    
    infra/builder/env.sh:
    + Added an `EVSSIM_SSH_KEYS_PATH` environmental variable for the path of
      the ssh keys.
    
    docker/image-maker.sh:
    * Split `PUBLIC_KEY` into `PUBLIC_KEY_RSA` and `PUBLIC_KEY_ED25519`,
      both are now written into `/root/.ssh/authorized_keys` and
      `~/.ssh/authorized_keys` inside the guest VM.
    
    builder.sh:
    * Do `chmod 600` on both keys.
    * In `evssim_guest()` - Add a `-i` option to the `ssh` command with the
      new Ed25519 key, with a secondary `-i` with the old RSA key as a
      fallthrough.
    * In `evssim_guest_copy()` - Add a `-i` option to the `scp` command with
      the new Ed25519 key, with a secondary `-i` with the old RSA key as a
      fallthrough.
    * In `ssh` and `scp` commands - Added `ssh-ed25519` to the list of
      explicitly accepted key types.
    
    docker-run-sanity.sh:
    * Add an SSH test that checks both(RSA, Ed25519) keys independantly,
      notifying the user whether each key works. If neither key works,
      displays an error and exits with an error code.
    
    Change-Id: I173df056b1d84de783da93efafdeaca0535dd7fc
    Closes: https://trello.com/c/PhXDgX4f

--- infra/builder/docker-run-sanity.sh
+++ infra/builder/docker-run-sanity.sh
@@ -7,6 +7,36 @@ evssim_qemu_fresh_image
 # Run qemu
 evssim_qemu_detached
 
+# Check SSH keys:
+try_ssh_key () {
+	local key="$1"
+	# true always returns 0
+	ssh -q -i "$key" -p 2222 -o ConnectionAttempts=1024 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PasswordAuthentication=no -o PubkeyAcceptedKeyTypes=+ssh-rsa,ssh-ed25519 $EVSSIM_QEMU_UBUNTU_USERNAME@localhost true
+}
+
+ssh_key_works=false
+ssh_keys_relative=$(realpath "$EVSSIM_SSH_KEYS_PATH" --relative-to .)
+# Try Ed25519 Key:
+if try_ssh_key "$EVSSIM_SSH_KEYS_PATH/id_ed25519"; then
+	ssh_key_works=true
+	echo "SSH: Authentication using Ed25519 key($ssh_keys_relative/id_ed25519) is succesfull"
+else
+	echo "SSH: WARNING - Authentication using Ed25519 key($ssh_keys_relative/id_ed25519) failed"
+fi
+# Try RSA Key:
+if try_ssh_key "$EVSSIM_SSH_KEYS_PATH/id_rsa"; then
+	ssh_key_works=true
+	echo "SSH: Authentication using RSA key($ssh_keys_relative/id_rsa) is succesfull"
+else
+	echo "SSH: WARNING - Authentication using RSA key($ssh_keys_relative/id_rsa) failed"
+fi
+# Exit if neither key works:
+if [ "$ssh_key_works" = "true" ]; then
+	echo "SSH: Authentication using at least one key works"
+else
+	echo "SSH: ERROR - Failure to authenticate using any of the keys"
+	exit 1
+fi
 
 # evssim_guest ls -al /dev/nvme0n1 2>/dev/null >/dev/null
 # Before checking if device nvme0n1 exists we should wait
