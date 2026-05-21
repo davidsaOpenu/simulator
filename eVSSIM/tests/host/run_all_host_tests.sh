b0e6bb36df5f61efa462c2d149bcbb78947e117f
Yoav Silver <silveryoav@gmail.com>
2026-05-21 11:52:30 +0300

    Epic: ONFI Multithread Maman11: Set up eVSSIM and add onfi_manager_threads to config
    Trello: https://trello.com/c/aoe8dpv2/83-set-up-evssim-env-add-number-of-threads-for-onfi-calls-to-ssdconftemplate
    
    Main changes:
    - Add onfi_manager_threads to sdd config and load it from the configuration file
    - Add ssd_config_tests.cc to host simulation tests
    - Add onfi_manager_threads to BaseTest and to SSDConf
    
    Suggested updates:
    - eVSSIM/LOG_MGR/logging_offline_analyzer.c: In extract_log_size(), call json_object_put(parsed)
    after using its child fields. Fix typo while checking fseek's output
    - eVSSIM/tests/host/run_all_host_tests.sh: Run unittest before the simulation tests
    - infra/builder/builder.sh: Update python to python3
    - .gitignore: add bulid files
    
    Change-Id: Ic861cfc1bfada35908808eb6ee2399ae3aa6995e

--- eVSSIM/tests/host/run_all_host_tests.sh
+++ eVSSIM/tests/host/run_all_host_tests.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-valgrind --leak-check=full --error-exitcode=2 ./simulation_tests_main --ci
 valgrind --leak-check=full --error-exitcode=2 ./unit_tests_main --ci
+valgrind --leak-check=full --error-exitcode=2 ./simulation_tests_main --ci
