6fc266360c07b7c244ca9d0496db316cb31bdcb9
michaelsomekh1 <michaelsomekh1@gmail.com>
2026-05-22 13:31:19 +0000

    Kernel compilation by configured kernel branch
    In the past the kernel was always compiled on the master branch. Now the kernel will be compiled using the container and branch from the config.json file.
    
    * The kernel dist folder is dynamically set based on the kernel branch  by parsing the Makefile instead of being hardcoded to 5.0.0+ and enforced by setting the variable KERNELRELEASE.
    
    * The dockerfile for ubuntu 26.04 was taken from an existing change: 1237623.
    
    * The entrypoint for the docker was changed to verify only qemu virtualization due to errors.
    
    * This change request was based on the change 1237496.
    
    
    Change-Id: I209375b09da87d4be7b86366e4d4c25c59655081

--- infra/builder/compile-kernel.sh
+++ infra/builder/compile-kernel.sh
@@ -11,8 +11,14 @@ mkdir -p $EVSSIM_DIST_FOLDER/kernel
 install_path=$EVSSIM_DOCKER_ROOT_PATH/$EVSSIM_DIST_FOLDER/kernel/
 initrd_path=$EVSSIM_DOCKER_ROOT_PATH/$EVSSIM_DIST_FOLDER/kernel/initrd.img-$EVSSIM_KERNEL_DIST
 
-evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make $EVSSIM_KCONFIG -j\`nproc\`"
-evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make $EVSSIM_KCONFIG modules -j\`nproc\`"
-evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make $EVSSIM_KCONFIG INSTALL_PATH=$install_path INSTALL_MOD_PATH=$install_path modules_install install"
-EVSSIM_RUN_SUDO=y evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make modules_install install && mkinitramfs -o $initrd_path $EVSSIM_KERNEL_DIST"
+GIT_CLEAN_RESET="git clean -fdx && git reset --hard HEAD"
+
+evssim_run_at_folder $EVSSIM_KERNEL_FOLDER $GIT_CLEAN_RESET
+evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "git checkout $EVSSIM_KERNEL_BRANCH"
+evssim_run_at_folder $EVSSIM_KERNEL_FOLDER $GIT_CLEAN_RESET
+evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make KERNELRELEASE=$EVSSIM_KERNEL_DIST $EVSSIM_KCONFIG defconfig"
+evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make KERNELRELEASE=$EVSSIM_KERNEL_DIST -j\`nproc\`"
+evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make KERNELRELEASE=$EVSSIM_KERNEL_DIST modules -j\`nproc\`"
+evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make KERNELRELEASE=$EVSSIM_KERNEL_DIST INSTALL_PATH=$install_path INSTALL_MOD_PATH=$install_path modules_install install"
+EVSSIM_RUN_SUDO=y evssim_run_at_folder $EVSSIM_KERNEL_FOLDER "make KERNELRELEASE=$EVSSIM_KERNEL_DIST modules_install install && mkinitramfs -o $initrd_path $EVSSIM_KERNEL_DIST"
 EVSSIM_RUN_SUDO=y evssim_run_at_folder $EVSSIM_KERNEL_FOLDER chown -R external:external $initrd_path
