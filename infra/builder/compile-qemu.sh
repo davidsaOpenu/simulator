e4a28388b063ce1e89e2ca9cc48497f2873ac7f0
Noam Tsalisher <noamtsalisher@yahoo.com>
2026-05-29 10:39:38 +0300

    Set up eVSSIM env + compile 10.0
    - This change sets the project up for ubuntu 26.04 usage as the guest os for qemu compilation.
    - It creates, in run-ci.sh, docker images for the old 14.04 image, and the newer 26.04 image, with all relevant changes necessary for a successful compilation in both images.
    - It then uses the images in run-ci.sh, with a specific order (check README.txt for information).
    - It is now mandatory to provde a specific ubuntu version to build-docker-image.sh and compile-qemu.sh; e.g 'compile-qemu.sh 14.04'.
    - Dockerfile separation is done via directories - versions/containers/<base_image_ver>/Dockerfile.
    - docker/entrypoint.sh is now checking only qemu relevant virtualization spec, with 'virt-host-validate qemu': lxc is irrelevant & fails builds since set -e wasn't set prior to this.
    - All scripts touched are now ran with set -e to exit on failure of any command.
    - Fetch remote updates
    - Fast-forward master/11.0 before compilation
    - Keep existing artifact cleanup logic
    - Avoid stale branch compilation failures
    
    Change-Id: I456ca984eac5fbb095de3f9a3ae1c57165c79406

--- infra/builder/compile-qemu.sh
+++ infra/builder/compile-qemu.sh
@@ -1,19 +1,60 @@
 #!/bin/bash
+set -e
+
 source ./builder.sh
+
+UBUNTU_VERSION="$1"
+
+# Setup git branch, and cleanup artifacts on host
+git -C "$EVSSIM_ROOT_PATH/$EVSSIM_QEMU_FOLDER" fetch --all --prune
+case "$UBUNTU_VERSION" in
+	14.04)
+		# Leave untagged target image to use and make sure we use correct branch for 14.04
+		git -C "$EVSSIM_ROOT_PATH/$EVSSIM_QEMU_FOLDER" checkout master
+		# this option is not set for qemu 11.0 branch
+		VSSIM_CONFIGURE_ARGS="--enable-vssim"
+		;;
+    	26.04)
+		# Tag image target name as evssim:26.04 and use 11.0 branch for 26.04
+		export EVSSIM_DOCKER_IMAGE_NAME="$EVSSIM_DOCKER_IMAGE_NAME:$UBUNTU_VERSION"
+		echo "INFO switching qemu source tree to branch 11.0 for Ubuntu 26.04 build"
+		git -C "$EVSSIM_ROOT_PATH/$EVSSIM_QEMU_FOLDER" checkout 11.0
+		# no --enable-vssim option for 11.0 branch
+		VSSIM_CONFIGURE_ARGS=""
+		;;
+	*)
+        	echo "ERROR unsupported qemu compile version: $UBUNTU_VERSION"
+        	echo "Usage: $0 <14.04|26.04>"
+        	exit 1
+        ;;
+esac
+
+# Necessary since builds interfere with each other due to artifacts
+git -C "$EVSSIM_ROOT_PATH/$EVSSIM_QEMU_FOLDER" pull --ff-only # !!!! THIS WILL FAIL FOR CHANGES IN THE QEMU BRANCH !!!!
+git -C "$EVSSIM_ROOT_PATH/$EVSSIM_QEMU_FOLDER" reset --hard
+git -C "$EVSSIM_ROOT_PATH/$EVSSIM_QEMU_FOLDER" clean -fdx
+git -C "$EVSSIM_ROOT_PATH/$EVSSIM_QEMU_FOLDER" submodule update --init --recursive
 
 # Configure qemu
 evssim_run_at_folder $EVSSIM_QEMU_FOLDER ./configure \
     --enable-trace-backends=log \
     --disable-docs --enable-tools \
     --enable-linux-aio \
-    --enable-vssim \
     --disable-sdl --disable-gtk \
     --enable-kvm --target-list=x86_64-softmmu \
+    $VSSIM_CONFIGURE_ARGS \
     "--extra-cflags='-Wno-error=unused-but-set-variable -Wno-error=deprecated-declarations $COMPILATION_CFLAGS'"
 
 # Make
+if [ "$UBUNTU_VERSION" = "26.04" ]; then
+    # Note: build with ninja for 26.04
+    evssim_run_at_folder "$EVSSIM_QEMU_FOLDER" bear -- ninja -C build -j$(nproc)
+    echo "INFO 26.04 QEMU build complete. Skipping legacy open-osd/exofs build."
+    exit 0
+fi
+
 evssim_run_at_folder $EVSSIM_QEMU_FOLDER make clean
-evssim_run_at_folder $EVSSIM_QEMU_FOLDER bear -- make -j8
+evssim_run_at_folder $EVSSIM_QEMU_FOLDER bear -- make -j$(nproc)
 
 # Build osc-osd
 evssim_run_at_folder "$EVSSIM_SIMULATOR_FOLDER/eVSSIM/osc-osd" "make MK_PATH=. ARCH=x86_64 clean && \
@@ -32,3 +73,4 @@ evssim_run_at_folder "open-osd" "make KSRC=/code/kernel ARCH=x86_64 clean && \
 # Copy OSD emulation and exofs setup script into the dist directory
 evssim_run_at_folder "$EVSSIM_SIMULATOR_FOLDER/eVSSIM/scripts" "cp -r exofs \
     $EVSSIM_DOCKER_ROOT_PATH/$EVSSIM_DIST_FOLDER/exofs"
+
