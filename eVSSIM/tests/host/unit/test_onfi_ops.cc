d22351c5a3ddc21c11e8fab649525729c9dc34f8
Yuri Khordal <yuriKhordal@proton.me>
2026-06-02 19:11:11 +0300

    Update Host Tests to Ubuntu 26.04
    Migrating eVSSIM from Ubuntu 14.04 to 26.04 breaks host tests
    compilation. This commit starts the migration by adding a Ubuntu 26.04
    based docker image and fixing host tests compilation errors due to
    warnings to compile and pass under the 26.04 based image with newer
    compilation tools(and newer warnings).
    
    A new `Dockerfile` under `infra/builder/versions/containers/26.04/` was
    added, based on the 14.04 based `Dockerfile` with updated package
    names/versions. The 14.04 based image compiled libwebsockets version
    4.1. This version does not compile under 26.04 and for that reason in
    the new image libwebsockets v4.5 is compiled for use. Due to ABI changes
    between v4.5 and v4.1 the file some code needed to be changed in
    `log_server.c` (at `eVSSIM/MONITOR/SERVER/logging_server`) which is used
    in multiple **different** parts of the CI (linked from multiple places)
    which splits it into two versions: one for libwebsockets v4.1 that works
    with CI parts that use the 14.04 based docker image, and another for
    v4.5 that works with parts that use 26.04.
    
    The old `uthash.h` header (at `eVSSIM/FTL_SOURCE/PAGE_MAP/TOOLS`) now
    fails(`-Werror`) to compile with new Ubuntu 26.04 compilation tools.
    Updated the header with a newer version of `uthash.h` (1.9.9 -> 2.3.0)
    to pass compilation without errors.
    
    In `build.mk` for the host tests, library headers are now imported using
    `-isystem` instead of `-I` to avoid compilation errors due to libraries
    (which we don't control).
    
    Co-authored-by: Yuri Khordal <yuriKhordal@proton.me>
    Co-authored-by: Aviv Barak Etedgui <avivbarak01@gmail.com>
    Trello: https://trello.com/c/6QweJcS7
    Change-Id: I522e4dc3b8fc1c9f178206b690b39aa2f4f27729

--- eVSSIM/tests/host/unit/test_onfi_ops.cc
+++ eVSSIM/tests/host/unit/test_onfi_ops.cc
@@ -13,11 +13,11 @@ namespace onfi_ops_test
     };
 
     TEST_F(OnfiOpsTest, StatusRegisterSizeTest) {
-        ASSERT_EQ(sizeof(onfi_status_reg_t), 1);
+        ASSERT_EQ(sizeof(onfi_status_reg_t), 1u);
     }
 
     TEST_F(OnfiOpsTest, ParamterPageStructSizeTest) {
-        ASSERT_EQ(sizeof(onfi_param_page_t), 256);
+        ASSERT_EQ(sizeof(onfi_param_page_t), 256u);
     }
 
     TEST_F(OnfiOpsTest, OnfiUpdateStatusRegisterWorksCorrectly) {
@@ -25,19 +25,19 @@ namespace onfi_ops_test
         memset(&status, 0, sizeof(onfi_status_reg_t));
 
         _ONFI_UPDATE_STATUS_REGISTER(&status, ONFI_FAILURE);
-        ASSERT_EQ(status.FAIL, 1);
-        ASSERT_EQ(status.FAILC, 0);
+        ASSERT_EQ(status.FAIL, 1u);
+        ASSERT_EQ(status.FAILC, 0u);
 
         _ONFI_UPDATE_STATUS_REGISTER(&status, ONFI_FAILURE);
-        ASSERT_EQ(status.FAIL, 1);
-        ASSERT_EQ(status.FAILC, 1);
+        ASSERT_EQ(status.FAIL, 1u);
+        ASSERT_EQ(status.FAILC, 1u);
 
         _ONFI_UPDATE_STATUS_REGISTER(&status, ONFI_SUCCESS);
-        ASSERT_EQ(status.FAIL, 0);
-        ASSERT_EQ(status.FAILC, 1);
+        ASSERT_EQ(status.FAIL, 0u);
+        ASSERT_EQ(status.FAILC, 1u);
 
         _ONFI_UPDATE_STATUS_REGISTER(&status, ONFI_SUCCESS);
-        ASSERT_EQ(status.FAIL, 0);
-        ASSERT_EQ(status.FAILC, 0);
+        ASSERT_EQ(status.FAIL, 0u);
+        ASSERT_EQ(status.FAILC, 0u);
     }
 };
