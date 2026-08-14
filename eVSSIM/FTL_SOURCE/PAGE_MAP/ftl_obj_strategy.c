d2d3afd20173d982a2bec5179d72b6704b3e21e4
Michael Somekh <michaelsomekh1@gmail.com>
2026-08-14 12:40:18 +0300

    Fix FTL page aligned and offset obj read
    Change-Id: I7b4b65428b6d509a6834905362b653a3f64e58ac

--- eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_obj_strategy.c
+++ eVSSIM/FTL_SOURCE/PAGE_MAP/ftl_obj_strategy.c
@@ -205,14 +205,13 @@ ftl_ret_val _FTL_OBJ_READ(uint8_t device_index, obj_id_t obj_loc, void *data, of
     if (data != NULL) {
         uint64_t outlen = 0;
         osd_ret = osd_read(OSD_DEVICE(device_index), obj_loc.partition_id, obj_loc.object_id,
-                    length, 0, NULL, data, &outlen, 0, OSD_SENSE(device_index), DDT_CONTIG);
+                    length, offset, NULL, data, &outlen, 0, OSD_SENSE(device_index), DDT_CONTIG);
         if (osd_ret < 0) {
             PDBG_FTL("osd_read failed with ret: %d.\n", osd_ret);
             return FTL_FAILURE;
         }
 
-        *p_length = get_ntohll(OSD_SENSE(device_index) + OSD_READ_VALUE_OFFSET);
-        if (length < *p_length) *p_length = length;
+        *p_length = outlen;
 
         memset(OSD_SENSE(device_index), 0x0, OSD_SENSE_BUFFER_SIZE);
     }
